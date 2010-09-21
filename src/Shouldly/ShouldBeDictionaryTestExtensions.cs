a3815d7fbc1369dad363f8e4aacc62507491a3ed
robconery <rob@wekeroad.com>
2010-09-20 17:36:14 -1000

    Added support for Dictionary key and value contains ...

---
+++ src/Shouldly/ShouldBeDictionaryTestExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace Shouldly {
+
+
+    [DebuggerStepThrough]
+    [ShouldlyMethods]
+    public static class ShouldBeDictionaryTestExtensions {
+        public static void ShouldContainKey<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key) {
+            if (!dictionary.ContainsKey(key))
+                throw new AssertionException(new ShouldlyMessage(key).ToString());
+
+        }
+        public static void ShouldContainKeyAndValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue val) {
+
+            if (!dictionary.ContainsKey(key))
+                throw new AssertionException(new ShouldlyMessage(key).ToString());
+
+            if (!dictionary[key].Equals(val))
+                throw new AssertionException(new ShouldlyMessage(val).ToString());
+
+
+        }
+        public static void ShouldNotContainKey<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key) {
+            if (dictionary.ContainsKey(key))
+                throw new AssertionException(new ShouldlyMessage(key).ToString());
+
+        }
+        public static void ShouldNotContainValueForKey<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue val) {
+
+            if (!dictionary.ContainsKey(key))
+                throw new AssertionException(new ShouldlyMessage(key).ToString());
+
+            if (dictionary[key].Equals(val))
+                throw new AssertionException(new ShouldlyMessage(val).ToString());
+
+        }
+    }
+
+
+}
---
+++ src/Tests/ShouldBeDictionaryTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using Shouldly;
+using System.Collections.Generic;
+
+namespace Tests {
+    [TestFixture]
+    public class ShouldBeDictionaryTests {
+        
+        [Test]
+        public void ShouldContainKey_WhenTrue_ShouldNotThrow() {
+            var dictionary = new Dictionary<string, string>();
+            dictionary.Add("key", "value");
+            dictionary.ShouldContainKey("key");
+            dictionary.ShouldNotContainKey("rob");
+        }
+        [Test]
+        public void ShouldNotContainKey_WhenTrue_ShouldNotThrow() {
+            var dictionary = new Dictionary<string, string>();
+            dictionary.Add("key", "value");
+            dictionary.ShouldNotContainKey("rob");
+        }
+        [Test]
+        public void ShouldContainKeyAndValue_WhenTrue_ShouldNotThrow() {
+            var dictionary = new Dictionary<string, string>();
+            dictionary.Add("key", "value");
+            dictionary.ShouldContainKeyAndValue("key","value");
+
+        }
+        [Test]
+        public void ShouldNotContainValueForKey_WhenTrue_ShouldNotThrow() {
+            var dictionary = new Dictionary<string, string>();
+            dictionary.Add("key", "value");
+            dictionary.ShouldNotContainValueForKey("key", "slurpee");
+
+        }
+        [Test]
+        public void ShouldContainKeyAndValue_WorkWithGuids() {
+            var guiddy = Guid.NewGuid();
+            var dictionary = new Dictionary<string, Guid>();
+            dictionary.Add("key", guiddy);
+            dictionary.ShouldContainKeyAndValue("key", guiddy);
+        }
+    }
+}
