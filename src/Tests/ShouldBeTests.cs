14103db7f86dd25d70c3dc11c3938582f77a7bbe
pvanderwoude <spurrymoses@gmail.com>
2010-10-12 18:05:21 +1100

    Added negative tests for ShouldBeLess/Greater/OrEqualTo - ie that they fail when the condition is false (as well as succeed when they are true)

--- src/Tests/ShouldBeTests.cs
+++ src/Tests/ShouldBeTests.cs
@@ -4,57 +4,60 @@ using Shouldly;
 
 namespace Tests
 {
-    [TestFixture]
-    public class ShouldBeTests
-    {
-        [Test]
-        public void ShouldBe_WhenTrue_ShouldNotThrow()
-        {
-            true.ShouldBe(true);
-        }
+	[TestFixture]
+	public class ShouldBeTests
+	{
+		[Test]
+		public void ShouldBe_WhenTrue_ShouldNotThrow()
+		{
+			true.ShouldBe(true);
+		}
 
-        [Test]
-        public void ShouldBe_WhenFalse_ShouldThrow()
-        {
-            "this string".ShouldNotBe("some other string");
-        }
+		[Test]
+		public void ShouldBe_WhenFalse_ShouldThrow()
+		{
+			"this string".ShouldNotBe("some other string");
+		}
 
-        [Test]
-        public void Should_WithNumbers_ShouldAllowTolerance()
-        {
-            Math.PI.ShouldNotBe(3.14);
-            Math.PI.ShouldBe(3.14, 0.01);
-            ((float)Math.PI).ShouldBe(3.14f, 0.01);
-        }
+		[Test]
+		public void Should_WithNumbers_ShouldAllowTolerance()
+		{
+			Math.PI.ShouldNotBe(3.14);
+			Math.PI.ShouldBe(3.14, 0.01);
+			((float)Math.PI).ShouldBe(3.14f, 0.01);
+		}
 
-        [Test]
-        public void ShouldBe_GreaterThan()
-        {
-            7.ShouldBeGreaterThan(1);
-            1.ShouldBeLessThan(7);
-        }
+		[Test]
+		public void ShouldBe_GreaterThan()
+		{
+			7.ShouldBeGreaterThan(1);
+			1.ShouldBeLessThan(7);
+			Assert.Throws<AssertionException>(() => 7.ShouldBeLessThan(0));
+			Assert.Throws<AssertionException>(() => 0.ShouldBeGreaterThan(7));
+		}
 
-        [Test]
-        public void ShouldBe_GreaterThanOrEqualTo()
-        {
-            7.ShouldBeGreaterThanOrEqualTo(1);
-            1.ShouldBeGreaterThanOrEqualTo(1);
-        }
+		[Test]
+		public void ShouldBe_GreaterThanOrEqualTo()
+		{
+			7.ShouldBeGreaterThanOrEqualTo(1);
+			1.ShouldBeGreaterThanOrEqualTo(1);
+			Assert.Throws<AssertionException>(() => 0.ShouldBeGreaterThanOrEqualTo(1));
+		}
 
-        [Test]
-        public void ShouldBeTypeOf_ShouldNotThrowForStrings() 
-        {
-            "Sup yo".ShouldBeTypeOf(typeof(string));
-        }
+		[Test]
+		public void ShouldBeTypeOf_ShouldNotThrowForStrings() 
+		{
+			"Sup yo".ShouldBeTypeOf(typeof(string));
+		}
 
-        class MyBase{ }
-        class MyThing : MyBase { }
+		class MyBase{ }
+		class MyThing : MyBase { }
 
-        [Test]
-        public void ShouldBeTypeOf_ShouldNotThrowForInheritance() 
-        {
-            new MyThing().ShouldBeTypeOf(typeof(MyBase));
-        }
+		[Test]
+		public void ShouldBeTypeOf_ShouldNotThrowForInheritance() 
+		{
+			new MyThing().ShouldBeTypeOf(typeof(MyBase));
+		}
 
-    }
+	}
 }
