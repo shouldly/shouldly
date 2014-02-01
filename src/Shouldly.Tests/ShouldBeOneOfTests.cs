d9939dabce4d209360f5f6d3a1a7af4fd8204570
Ben Scott <ben.scott@readify.net>
2014-02-01 19:01:20 +1000

    #58 Naive implementation of ShouldBeOneOf

---
+++ src/Shouldly.Tests/ShouldBeOneOfTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Shouldly.Tests
+{
+    public class ShouldBeOneOfTests
+    {
+        [Test]
+        public void ShouldNotThrowWhenValueIsExpected()
+        {
+            1.ShouldBeOneOf(1, 2, 3);
+        }
+
+        [Test]
+        public void ShouldThrowWhenValueIsNotExpected()
+        {
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => 1.ShouldBeOneOf(4, 5, 6));
+        }
+    }
+}
---
+++ src/Shouldly.Tests/ShouldNotBeOneOfTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Shouldly.Tests
+{
+    public class ShouldNotBeOneOfTests
+    {
+        [Test]
+        public void ShouldThrowWhenValueIsExpected()
+        {
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => 1.ShouldNotBeOneOf(1, 2, 3));
+        }
+
+        [Test]
+        public void ShouldNotThrowWhenValueIsNotExpected()
+        {
+            1.ShouldNotBeOneOf(4, 5, 6);
+        }
+    }
+}
