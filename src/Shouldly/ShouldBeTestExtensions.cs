71ec72205da744d2fe5c8e6997c5ef7ff7611e27
Ben Scott <ben.scott@readify.net>
2014-02-01 19:09:47 +1000

    Nicer error messages for ShouldBeOneOf and ShouldNotBeOneOf

--- src/Shouldly/ShouldBeTestExtensions.cs
+++ src/Shouldly/ShouldBeTestExtensions.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using NUnit.Framework;
 using System;
+using System.Linq;
 
 namespace Shouldly
 {
@@ -97,12 +98,14 @@ namespace Shouldly
 
         public static void ShouldBeOneOf<T>(this T actual, params T[] expected)
         {
-            expected.ShouldContain(actual);
+            if (!expected.Contains(actual))
+                throw new ChuckedAWobbly(new ShouldlyMessage(expected, actual).ToString());
         }
 
         public static void ShouldNotBeOneOf<T>(this T actual, params T[] expected)
         {
-            expected.ShouldNotContain(actual);
+            if (expected.Contains(actual))
+                throw new ChuckedAWobbly(new ShouldlyMessage(expected, actual).ToString());
         }
     }
 }
