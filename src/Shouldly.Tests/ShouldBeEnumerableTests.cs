c77264abe545b85f87f25cf12426a2a92dad9b36
ilya-murzinov <murz42@gmail.com>
2014-02-11 12:01:35 +0400

    Added tests for ShouldContain and ShouldNotContain in Shouldly.Tests/ShouldBeEnumerableTests.cs

--- src/Shouldly.Tests/ShouldBeEnumerableTests.cs
+++ src/Shouldly.Tests/ShouldBeEnumerableTests.cs
@@ -9,13 +9,123 @@ namespace Shouldly.Tests
         [Test]
         public void ShouldContain_WhenTrue_ShouldNotThrow()
         {
-            new[] { 1, 2, 3 }.ShouldContain(2);
+            new object[] { }.ShouldContain(null);
+            new object[] { }.ShouldContain((object) null);
+
+            new int?[] {1, 2, 3, 4, 5}.ShouldContain((int?) null);
+            new[] {1, 2, 3, 4, 5}.ShouldContain(1);
+            new[] { 1, 2, 3, 4, 5 }.ShouldContain(5);
+            new[] { 1, 2, 3, 4, 5 }.ShouldContain(3);
+
+            new double?[] { }.ShouldContain((double?) null);
+            new[] { 1.0000001d }.ShouldContain(1.0000001d);
+            new[] { 1.0000001d }.ShouldContain(1d, 0.0001);
+            new[] { 1, 2, 3.000000000001d, 4, 5 }.ShouldContain(4);
+            new[] { 1, 2, 3.000000000001d, 4, 5 }.ShouldContain(3.000000000001d);
+            new[] { 1, 2, 3.000000000001d, 4, 5 }.ShouldContain(3.000001d, 0.00001);
+            new[] { 100.0001d, 2123.93812d, 3.9712354123d, 412.12354d, 5.00000000012d }.ShouldContain(412.12354d);
+            new[] { 100.0001d, 2123.93812d, 3.9712354123d, 412.12354d, 5.00000000012d }.ShouldContain(5.003333333d, 0.01);
+
+            new float?[] { }.ShouldContain((float?)null);
+            new[] { 1.0000001f }.ShouldContain(1.0000001f);
+            new[] { 1.0000001f }.ShouldContain(1f, 0.0001);
+            new[] { 1, 2, 3.000000000001f, 4, 5 }.ShouldContain(4);
+            new[] { 1, 2, 3.000000000001f, 4, 5 }.ShouldContain(3f);
+            new[] { 100.0001d, 2123.93812d, 3.9712354123d, 412.12354f, 5.00000000012d }.ShouldContain(412.123540000000001f);
+
+            new[] {""}.ShouldContain((string) null);
+            new[] {""}.ShouldContain("");
+            new[] {" "}.ShouldContain(" ");
+            new[] { "1" }.ShouldContain("1");
+            new[] { "1", "2", "3", "4", "5" }.ShouldContain("1");
+            new[] { "1", "2", "3", "4", "5" }.ShouldContain("5");
+            new[] { "1", "2", "3", "4", "5" }.ShouldContain("3");
+        }
+
+        [Test]
+        public void ShouldContain_WhenFalse_ShouldThrow()
+        {
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1, 2, 3, 4, 5 }.ShouldContain(6));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1, 2, 3, 4, 5 }.ShouldContain(123123));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1, 2, 3, 4, 5 }.ShouldContain(-123643));
+
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1.0000001d }.ShouldContain(1.00000001d));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1.0000001d }.ShouldContain(1d, 0.0000000001));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1, 2, 3.000000000001d, 4, 5 }.ShouldContain(40000));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1, 2, 3.000000000001d, 4, 5 }.ShouldContain(3.00000001d));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1, 2, 3.000000000001d, 4, 5 }.ShouldContain(3.000001d, 0.000000000000001));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 100.0001d, 2123.93812d, 3.9712354123d, 412.12354d, 5.00000000012d }.ShouldContain(412.123541d));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 100.0001d, 2123.93812d, 3.9712354123d, 412.12354d, 5.00000000012d }.ShouldContain(5.003333333d, 0.0001));
+
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1.0000001f }.ShouldContain(1.001f));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1.0000001f }.ShouldContain(1f, 0.000000001));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1, 2, 3.000000000001f, 4, 5 }.ShouldContain(14));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1, 2, 3.000000000001f, 4, 5 }.ShouldContain(3.1f));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 100.0001d, 2123.93812d, 3.9712354123d, 412.12354f, 5.00000000012d }.ShouldContain(412.12365f));
+
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { "" }.ShouldContain(" "));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { " " }.ShouldContain(""));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { "1" }.ShouldContain("2"));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { "1", "2", "3", "4", "5" }.ShouldContain("1.1"));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { "1", "2", "3", "4", "5" }.ShouldContain("501320123684721352134213452148172634987129380478123478612"));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { "1", "2", "3", "4", "5" }.ShouldContain("3sssasddedwe"));
         }
 
         [Test]
         public void ShouldNotContain_WhenTrue_ShouldNotThrow()
         {
-            new[] { 1, 2, 3 }.ShouldNotContain(5);
+            new[] { 1, 2, 3, 4, 5 }.ShouldNotContain(6);
+            new[] { 1, 2, 3, 4, 5 }.ShouldNotContain(123123);
+            new[] { 1, 2, 3, 4, 5 }.ShouldNotContain(-123643);
+
+            new[] { 1.0000001d }.ShouldNotContain(1.00000001d);
+            new[] { 1, 2, 3.000000000001d, 4, 5 }.ShouldNotContain(40000);
+            new[] { 1, 2, 3.000000000001d, 4, 5 }.ShouldNotContain(3.00000001d);
+            new[] { 100.0001d, 2123.93812d, 3.9712354123d, 412.12354d, 5.00000000012d }.ShouldNotContain(412.123541d);
+            
+            new[] { 1.0000001f }.ShouldNotContain(1.001f);
+            new[] { 1, 2, 3.000000000001f, 4, 5 }.ShouldNotContain(14);
+            new[] { 1, 2, 3.000000000001f, 4, 5 }.ShouldNotContain(3.1f);
+            new[] { 100.0001d, 2123.93812d, 3.9712354123d, 412.12354f, 5.00000000012d }.ShouldNotContain(412.12365f);
+
+            new[] { "" }.ShouldNotContain(" ");
+            new[] { " " }.ShouldNotContain("");
+            new[] { "1" }.ShouldNotContain("2");
+            new[] { "1", "2", "3", "4", "5" }.ShouldNotContain("1.1");
+            new[] { "1", "2", "3", "4", "5" }.ShouldNotContain("501320123684721352134213452148172634987129380478123478612");
+            new[] { "1", "2", "3", "4", "5" }.ShouldNotContain("3sssasddedwe");
+        }
+
+        [Test]
+        public void ShouldNotContain_WhenFalse_ShouldThrow()
+        {
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new object[] { }.ShouldNotContain(null));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new object[] { }.ShouldNotContain((object)null));
+
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new int?[] { 1, 2, 3, 4, 5 }.ShouldNotContain((int?)null));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1, 2, 3, 4, 5 }.ShouldNotContain(1));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1, 2, 3, 4, 5 }.ShouldNotContain(5));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1, 2, 3, 4, 5 }.ShouldNotContain(3));
+
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new double?[] { }.ShouldNotContain((double?)null));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1.0000001d }.ShouldNotContain(1.0000001d));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1, 2, 3.000000000001d, 4, 5 }.ShouldNotContain(4));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1, 2, 3.000000000001d, 4, 5 }.ShouldNotContain(3.000000000001d));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 100.0001d, 2123.93812d, 3.9712354123d, 412.12354d, 5.00000000012d }.ShouldNotContain(412.12354d));
+            
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new float?[] { }.ShouldNotContain((float?)null));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1.0000001f }.ShouldNotContain(1.0000001f));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1, 2, 3.000000000001f, 4, 5 }.ShouldNotContain(4));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 1, 2, 3.000000000001f, 4, 5 }.ShouldNotContain(3f));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { 100.0001d, 2123.93812d, 3.9712354123d, 412.12354f, 5.00000000012d }.ShouldNotContain(412.123540000000001f));
+
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { "" }.ShouldNotContain((string)null));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { "" }.ShouldNotContain(""));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { " " }.ShouldNotContain(" "));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { "1" }.ShouldNotContain("1"));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { "1", "2", "3", "4", "5" }.ShouldNotContain("1"));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { "1", "2", "3", "4", "5" }.ShouldNotContain("5"));
+            Shouldly.Should.Throw<ChuckedAWobbly>(() => new[] { "1", "2", "3", "4", "5" }.ShouldNotContain("3"));
         }
 
         [Test]
