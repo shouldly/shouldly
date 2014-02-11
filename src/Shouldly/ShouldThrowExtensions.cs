86ad3adaa76337ec212c4edcf6b734a6d694a5cb
Jake Ginnivan <jake.ginnivan@readify.net>
2014-02-11 13:21:37 +0000

    Fixing build error

---
+++ src/Shouldly/ShouldThrowExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Shouldly
+{
+    [DebuggerStepThrough]
+    [ShouldlyMethods]
+    public static partial class Should
+    {
+        public static TException Throw<TException>(Action actual) where TException : Exception
+        {
+            try
+            {
+                actual();
+            }
+            catch (TException e)
+            {
+                return e;
+            }
+            catch (Exception e)
+            {
+                throw new ChuckedAWobbly(new ShouldlyMessage(typeof(TException), e.GetType()).ToString());
+            }
+
+            throw new ChuckedAWobbly(new ShouldlyMessage(typeof(TException)).ToString());
+        }
+
+        public static void NotThrow(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception ex)
+            {
+                throw new ChuckedAWobbly(new ShouldlyMessage(ex.GetType()).ToString());
+            }
+        }
+
+        public static T NotThrow<T>(Func<T> action)
+        {
+            try
+            {
+                return action();
+            }
+            catch (Exception ex)
+            {
+                throw new ChuckedAWobbly(new ShouldlyMessage(ex.GetType()).ToString());
+            }
+        }
+    }
+}
--- src/Shouldly/ShouldThrowTaskExtensions.cs
+++ src/Shouldly/ShouldThrowTaskExtensions.cs
@@ -1,31 +1,11 @@
-﻿using System;
-using System.Diagnostics;
+﻿#if net40
+using System;
 using System.Threading.Tasks;
 
 namespace Shouldly
 {
-    [DebuggerStepThrough]
-    [ShouldlyMethods]
-    public static class Should
+    public static partial class Should
     {
-        public static TException Throw<TException>(Action actual) where TException : Exception
-        {
-            try
-            {
-                actual();
-            }
-            catch (TException e)
-            {
-                return e;
-            }
-            catch (Exception e)
-            {
-                throw new ChuckedAWobbly(new ShouldlyMessage(typeof(TException), e.GetType()).ToString());
-            }
-
-            throw new ChuckedAWobbly(new ShouldlyMessage(typeof(TException)).ToString());
-        }
-        
         public static TException Throw<TException>(Func<Task> actual) where TException : Exception
         {
             try
@@ -51,18 +31,6 @@ namespace Shouldly
             throw new ChuckedAWobbly(new ShouldlyMessage(typeof(TException)).ToString());
         }
 
-        public static void NotThrow(Action action)
-        {
-            try
-            {
-                action();
-            }
-            catch (Exception ex)
-            {
-                throw new ChuckedAWobbly(new ShouldlyMessage(ex.GetType()).ToString());
-            }
-        }
-
         public static void NotThrow(Func<Task> action)
         {
             try
@@ -72,18 +40,6 @@ namespace Shouldly
             catch (AggregateException ex)
             {
                 throw new ChuckedAWobbly(new ShouldlyMessage(ex.InnerException.GetType()).ToString());
-            }
-            catch (Exception ex)
-            {
-                throw new ChuckedAWobbly(new ShouldlyMessage(ex.GetType()).ToString());
-            }
-        }
-
-        public static T NotThrow<T>(Func<T> action)
-        {
-            try
-            {
-                return action();
             }
             catch (Exception ex)
             {
@@ -108,3 +64,4 @@ namespace Shouldly
         }
     }
 }
+#endif
