6c29deca2ca294a79c174c6c3e549732d5bbc727
Gert Jansen van Rensburg <gertjvr@gmail.com>
2014-03-03 21:24:24 +1000

    Initial implementation of Should.CompleteIn(...)

---
+++ src/Shouldly.Tests/ShouldCompleteInTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Shouldly.Tests
+{
+    [TestFixture]
+    public class ShouldCompleteInTests
+    {
+        [Test]
+        public void ShouldCompleteIn_WhenFinishBeforeTimeout()
+        {
+            Shouldly.Should.NotThrow(() => Shouldly.Should.CompleteIn(() => { Thread.Sleep(TimeSpan.FromSeconds(1)); }, TimeSpan.FromSeconds(2)));
+        }
+
+        [Test]
+        public void ShouldCompleteIn_WhenFinishAfterTimeout()
+        {
+            Shouldly.Should.Throw<TimeoutException>(() => Shouldly.Should.CompleteIn(() => { Thread.Sleep(TimeSpan.FromSeconds(2)); }, TimeSpan.FromSeconds(1)));
+        }
+
+        [Test]
+        public void ShouldCompleteIn_WhenThrowsNonTimeoutException()
+        {
+            Shouldly.Should.Throw<AggregateException>(() => Shouldly.Should.CompleteIn(() => { throw new NotImplementedException(); }, TimeSpan.FromSeconds(1)));
+        }
+    }
+}
---
+++ src/Shouldly/ShouldCompleteInExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+#if net40
+using System;
+using System.Threading.Tasks;
+
+namespace Shouldly
+{
+    public static partial class Should
+    {
+        public static void CompleteIn(Action action, TimeSpan timeout)
+        {
+            var actual = Task.Factory.StartNew(action);
+
+            CompleteIn(actual, timeout);
+        }
+
+        public static void CompleteIn<T>(Func<T> function, TimeSpan timeout)
+        {
+            var actual = Task.Factory.StartNew(function);
+
+            CompleteIn(actual, timeout);
+        }
+
+        public static void CompleteIn(Task actual, TimeSpan timeout)
+        {
+            try
+            {
+                actual.TimeoutAfter(timeout).Wait();
+            }
+            catch (AggregateException ae)
+            {
+                var te = ae.InnerExceptions.FirstOrDefault(e => e is TimeoutException);
+
+                throw te ?? ae;
+            }
+        }
+
+        public static void CompleteIn<T>(Task<T> actual, TimeSpan timeout)
+        {
+            try
+            {
+                actual.TimeoutAfter(timeout).Wait();
+            }
+            catch (AggregateException ae)
+            {
+                var te = ae.InnerExceptions.FirstOrDefault(e => e is TimeoutException);
+
+                throw te ?? ae;
+            }
+        }
+    }
+}
+#endif
