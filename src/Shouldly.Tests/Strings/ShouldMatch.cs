94728926dbdbd0e5305c35d8986bdf7fc69c78fe
Jake Ginnivan <jake.ginnivan@readify.net>
2014-03-06 14:15:39 +0000

    Should match converted

---
+++ src/Shouldly.Tests/Strings/ShouldMatch.cs
@@ -0,0 +1,20 @@
+﻿namespace Shouldly.Tests.Strings
+{
+    public class ShouldMatch : ShouldlyShouldTestScenario
+    {
+        protected override void ShouldThrowAWobbly()
+        {
+            "Cheese".ShouldMatch(@"\d+");
+        }
+
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get { return "\"Cheese\" should match \"\\d+\" but was \"Cheese\""; }
+        }
+
+        protected override void ShouldPass()
+        {
+            "Cheese".ShouldMatch(@"C.e{2}s[e]");
+        }
+    }
+}
