9bb94dec9760c82fda74373df823a13644189cf2
Jake Ginnivan <jake.ginnivan@readify.net>
2014-03-06 15:19:29 +0000

    Should contain tests migrated

---
+++ src/Shouldly.Tests/ShouldContain/EmptyArrayScenario.cs
@@ -0,0 +1,15 @@
+﻿namespace Shouldly.Tests.ShouldContain
+{
+    public class EmptyArrayScenario : ShouldlyShouldFailureTestScenario
+    {
+        protected override void ShouldThrowAWobbly()
+        {
+            new int[0].ShouldContain(1);
+        }
+
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get { return "new int[0] should contain 1 but was []"; }
+        }
+    }
+}
---
+++ src/Shouldly.Tests/ShouldContain/StringContainsCharScenario.cs
@@ -0,0 +1,20 @@
+﻿namespace Shouldly.Tests.ShouldContain
+{
+    public class StringContainsCharScenario : ShouldlyShouldTestScenario
+    {
+        protected override void ShouldThrowAWobbly()
+        {
+            "Foo".ShouldContain('B');
+        }
+
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get { return "\"Foo\" should contain B but was \"Foo\""; }
+        }
+
+        protected override void ShouldPass()
+        {
+            "Foo".ShouldContain('F');
+        }
+    }
+}
