893fb4a9dd11592da04e423d73da05fd2dfd0572
Jake Ginnivan <jake.ginnivan@readify.net>
2014-03-06 14:12:24 +0000

    ShouldContainWithoutWhitespace

--- src/Shouldly.Tests/ShouldBeStringTests.cs
+++ src/Shouldly.Tests/ShouldBeStringTests.cs
@@ -6,12 +6,6 @@ namespace Shouldly.Tests
     public class ShouldBeStringTests
     {
         [Test]
-        public void ShouldContainWithoutWhitespace_IsPrettyLoose()
-        {
-            "Fun   with space   and \"quotes\"".ShouldContainWithoutWhitespace("Fun with space and 'quotes'");
-        }
-
-        [Test]
         public void ShouldMatch_Should_Match_Based_On_RegEx_Pattern()
         {
             "Cheese".ShouldMatch(@"C.e{2}s[e]");
---
+++ src/Shouldly.Tests/Strings/ShouldContainWithoutWhitespace.cs
@@ -0,0 +1,22 @@
+﻿namespace Shouldly.Tests.Strings
+{
+    public class ShouldContainWithoutWhitespace : ShouldlyShouldTestScenario
+    {
+        protected override void ShouldThrowAWobbly()
+        {
+            "Fun   with space   and \"quotes\"".ShouldContainWithoutWhitespace("Fun with space and missing quotes");
+        }
+
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get { return "\"Fun   with space   and \\\"quotes\\\"\" " +
+                         "should contain without whitespace \"Fun with space and missing quotes\" " +
+                         "but was \"Fun   with space   and \"quotes\"\""; }
+        }
+
+        protected override void ShouldPass()
+        {
+            "Fun   with space   and \"quotes\"".ShouldContainWithoutWhitespace("Fun with space and 'quotes'");
+        }
+    }
+}
