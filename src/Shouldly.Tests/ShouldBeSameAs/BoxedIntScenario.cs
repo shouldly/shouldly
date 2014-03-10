62539443c6946d470e1321276ad7f53f3ea8a280
Jake Ginnivan <jake.ginnivan@readify.net>
2014-03-10 16:06:00 +0000

    Removed some duplicate tests and migrated last few missing tests

---
+++ src/Shouldly.Tests/ShouldBeSameAs/BoxedIntScenario.cs
@@ -0,0 +1,25 @@
+﻿using Shouldly.Tests.TestHelpers;
+
+namespace Shouldly.Tests.ShouldBeSameAs
+{
+    public class BoxedIntScenario : ShouldlyShouldTestScenario
+    {
+        private readonly object _boxedInt = 1;
+        private readonly object _differentBoxedInt = 1;
+
+        protected override void ShouldThrowAWobbly()
+        {
+            _boxedInt.ShouldBeSameAs(_differentBoxedInt);
+        }
+
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get { return "_boxedInt should be same as 1 but was 1"; }
+        }
+
+        protected override void ShouldPass()
+        {
+            _boxedInt.ShouldBeSameAs(_boxedInt);
+        }
+    }
+}
