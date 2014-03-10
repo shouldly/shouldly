67071137ed34ce5de62b9b787f28a413772f29b1
Jake Ginnivan <jake.ginnivan@readify.net>
2014-03-10 20:19:16 +0000

    Fixed build

--- src/Shouldly.Tests/ShouldThrow/NestedAwaitsDoNotDeadlockScenario.cs
+++ src/Shouldly.Tests/ShouldThrow/NestedAwaitsDoNotDeadlockScenario.cs
@@ -1,7 +1,7 @@
-﻿using System.Threading.Tasks;
-#if net40
+﻿#if net40
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace Shouldly.Tests.ShouldThrow
