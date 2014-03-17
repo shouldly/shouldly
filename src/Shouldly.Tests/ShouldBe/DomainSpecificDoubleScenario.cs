954bb795dd4ec278873451512c885fdf080ef174
Jake Ginnivan <jake.ginnivan@readify.net>
2014-03-17 20:17:35 +0000

    Brought back nUnits numeric equality checking

---
+++ src/Shouldly.Tests/ShouldBe/DomainSpecificDoubleScenario.cs
@@ -0,0 +1,26 @@
+﻿using Shouldly.Tests.TestHelpers;
+
+namespace Shouldly.Tests.ShouldBe
+{
+    public class DomainSpecificDoubleScenario : ShouldlyShouldTestScenario
+    {
+        protected override void ShouldThrowAWobbly()
+        {
+            object a = 1;
+            DomainSpecificDouble min = 0;
+            min.ShouldBe(a);
+        }
+
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get { return "min should be 1 but was 0"; }
+        }
+
+        protected override void ShouldPass()
+        {
+            object a = 0d;
+            DomainSpecificDouble min = 0;
+            min.ShouldBe(a);
+        }
+    }
+}
