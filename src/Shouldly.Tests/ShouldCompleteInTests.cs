847acd773450b1d22f9836bdb41b1e22ea404b10
Jake Ginnivan <jake.ginnivan@readify.net>
2014-03-22 11:01:55 +0000

    Do not throw aggregate exception unless we have to

--- src/Shouldly.Tests/ShouldCompleteInTests.cs
+++ src/Shouldly.Tests/ShouldCompleteInTests.cs
@@ -23,7 +23,7 @@ namespace Shouldly.Tests
         [Test]
         public void ShouldCompleteIn_WhenThrowsNonTimeoutException()
         {
-            Should.Throw<AggregateException>(() => Should.CompleteIn(() => { throw new NotImplementedException(); }, TimeSpan.FromSeconds(1)));
+            Should.Throw<NotImplementedException>(() => Should.CompleteIn(() => { throw new NotImplementedException(); }, TimeSpan.FromSeconds(1)));
         }
     }
 }
