e235e357060a8a8b2a767413956290d0ea0b7b6e
ilya-murzinov <murz42@gmail.com>
2014-03-27 17:52:21 +0400

    added tests for Should.ThrowExact<>

---
+++ src/Shouldly.Tests/ShouldThrow/FuncOfTaskExactExceptionScenario.cs
@@ -0,0 +1,41 @@
+﻿#if net40
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Shouldly.Tests.TestHelpers;
+
+namespace Shouldly.Tests.ShouldThrow
+{
+    public class FuncOfTaskExactExceptionScenario : ShouldlyShouldTestScenario
+    {
+        protected override void ShouldThrowAWobbly()
+        {
+            Should.ThrowExact<NullReferenceException>(() =>
+            {
+                var task = Task.Factory.StartNew(() => { }, 
+                    CancellationToken.None, TaskCreationOptions.None,
+                    TaskScheduler.Default);
+                return task;
+            });
+        }
+
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get { return "Should throw exact System.NullReferenceException but does not"; }
+        }
+
+        protected override void ShouldPass()
+        {
+            var ex = Should.ThrowExact<InvalidOperationException>(() =>
+            {
+                var task = Task.Factory.StartNew(() => { throw new InvalidOperationException(); },
+                    CancellationToken.None, TaskCreationOptions.None,
+                    TaskScheduler.Default);
+                return task;
+            });
+            ex.ShouldNotBe(null);
+            ex.ShouldBeOfType<InvalidOperationException>();
+        }
+    }
+}
+#endif
