042872815253cd9fccd94a20430153b96fd79506
ilya-murzinov <murz42@gmail.com>
2014-03-27 17:24:52 +0400

    added tests for Should.ThrowExact<>

---
+++ src/Shouldly.Tests/ShouldThrow/ActionDelegateExactExceptionThrowsDifferentExceptionScenario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Shouldly.Tests.TestHelpers;
+
+namespace Shouldly.Tests.ShouldThrow
+{
+    public class ActionDelegateExactExceptionThrowsDifferentExceptionScenario : ShouldlyShouldFailureTestScenario
+    {
+        protected override void ShouldThrowAWobbly()
+        {
+            Should.ThrowExact<ArgumentException>(() => { throw new ArgumentNullException(); });
+        }
+
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get { return "Should throw exact System.ArgumentException but was System.ArgumentNullException"; }
+        }
+    }
+}
