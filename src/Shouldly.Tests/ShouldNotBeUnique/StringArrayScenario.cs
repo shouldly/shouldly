f9c16e42057d9537456cf76664bb68c75225601a
Ilya Murzinov <murz42@gmail.com>
2014-09-19 00:27:15 +0400

    fix code formatting

--- src/Shouldly.Tests/ShouldNotBeUnique/StringArrayScenario.cs
+++ src/Shouldly.Tests/ShouldNotBeUnique/StringArrayScenario.cs
@@ -2,24 +2,25 @@
 
 namespace Shouldly.Tests.ShouldNotBeUnique
 {
-	public class StringArrayScenario : ShouldlyShouldTestScenario
-	{
-		protected override string ChuckedAWobblyErrorMessage {
-			get 
-			{
-				return "new string[] { \"string2\", \"string1\", \"string42\", \"string53\" } " +
-					"should not be unique [ \"string2\", \"string1\", \"string42\", \"string53\" ] but does";
-			}
-		}
+    public class StringArrayScenario : ShouldlyShouldTestScenario
+    {
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get
+            {
+                return "new string[] { \"string2\", \"string1\", \"string42\", \"string53\" } " +
+                "should not be unique [ \"string2\", \"string1\", \"string42\", \"string53\" ] but does";
+            }
+        }
 
-		protected override void ShouldThrowAWobbly()
-		{
-			new string[] { "string2", "string1", "string42", "string53" }.ShouldNotBeUnique();
-		}
+        protected override void ShouldThrowAWobbly()
+        {
+            new string[] { "string2", "string1", "string42", "string53" }.ShouldNotBeUnique();
+        }
 
-		protected override void ShouldPass()
-		{
-			new string[] { "string2", "string1", "string42", "string2" }.ShouldNotBeUnique();
-		}
-	}
+        protected override void ShouldPass()
+        {
+            new string[] { "string2", "string1", "string42", "string2" }.ShouldNotBeUnique();
+        }
+    }
 }
