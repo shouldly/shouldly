c627d85dbcec1b211d28ff38dd470d4e02ac49bf
Jake Ginnivan <jake@ginnivan.net>
2015-01-10 14:20:09 +0000

    Fixing issue #164

---
+++ src/Shouldly.Tests/ShouldBe/EnumerableType/IgnoreOrderIEnumerableMethodYieldReturn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Shouldly.Tests.TestHelpers;
+
+namespace Shouldly.Tests.ShouldBe.EnumerableType
+{
+    public class IgnoreOrderIEnumerableMethodYieldReturn : ShouldlyShouldTestScenario
+    {
+        protected override void ShouldPass()
+        {
+            GetEnumerable().ShouldBe(new[] {1}, true);
+        }
+
+        protected override void ShouldThrowAWobbly()
+        {
+            GetEnumerable().ShouldBe(new[] {1, 2}, true);
+        }
+
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get
+            {
+                return "GetEnumerable() should be [1, 2] (ignoring order) but GetEnumerable() is missing [2]";
+            }
+        }
+
+        private static IEnumerable<int> GetEnumerable()
+        {
+            yield return 1;
+        }
+    }
+}
