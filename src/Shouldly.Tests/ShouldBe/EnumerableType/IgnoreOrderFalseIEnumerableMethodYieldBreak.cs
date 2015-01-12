322e3a5c3f4443edf2874cca24ef0ad8e90d19b1
Chaitanya Gurrapu <Chaitanya.Gurrapu@gmail.com>
2015-01-12 12:31:49 +1000

    Added a few more unit tests while review'ing @JakeGinnivan 's pull request.

---
+++ src/Shouldly.Tests/ShouldBe/EnumerableType/IgnoreOrderFalseIEnumerableMethodYieldBreak.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Shouldly.Tests.TestHelpers;
+
+namespace Shouldly.Tests.ShouldBe.EnumerableType
+{
+    public class IgnoreOrderFalseIEnumerableMethodYieldBreak : ShouldlyShouldTestScenario
+    {
+        protected override void ShouldPass()
+        {
+            GetEmptyEnumerable().ShouldBe(new int[0], false);
+        }
+
+        protected override void ShouldThrowAWobbly()
+        {
+            GetEmptyEnumerable().ShouldBe(new[] {2, 4}, false);
+        }
+
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get
+            {
+                return "GetEmptyEnumerable() should be [2, 4] but was [] difference [*, *]";
+            }
+        }
+
+        private static IEnumerable<int> GetEmptyEnumerable()
+        {
+            yield break;
+        }
+    }
+}
