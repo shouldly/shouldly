241573dff15285e8573988c87e41a14c4c1b90f1
Uri Goldstein <uri.goldstein@gmail.com>
2015-02-22 12:33:37 +0200

    Added test scenario for ShouldContain in an array of objects. (Based on existing behavior)

---
+++ src/Shouldly.Tests/ShouldContain/ObjectScenario.cs
@@ -0,0 +1,36 @@
+﻿using Shouldly.Tests.TestHelpers;
+using System;
+
+namespace Shouldly.Tests.ShouldContain
+{
+    public class ObjectScenario : ShouldlyShouldTestScenario
+    {
+        protected override void ShouldThrowAWobbly()
+        {
+            var a = new Object();
+            var b = new Object();
+            var c = new Object();
+            var d = new Object();
+            new[] { a, b, c }.ShouldContain(d);
+        }
+
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get { return @"
+    new[] { a, b, c }
+        should contain
+    System.Object
+        but was
+    [System.Object, System.Object, System.Object]"; }
+            
+        }
+
+        protected override void ShouldPass()
+        {
+            var a = new Object();
+            var b = new Object();
+            var c = new Object();
+            new[] { a, b, c }.ShouldContain(b);
+        }
+    }
+}
