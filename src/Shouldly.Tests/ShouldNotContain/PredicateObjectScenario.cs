5529aa0634e37452bf8d7d5f38d0c807838d1142
Uri Goldstein <uri.goldstein@gmail.com>
2015-02-24 23:39:36 +0200

    Added test for ShouldContain and ShouldNotContain with predicates in the scenario where the arguments are objects with default ToString() implementation. (PS - This is handled by the default "ExpectedErrorMessage" in ShouldlyMessage.cs and not by ShouldContainATypeWithDefaultToStringMessageGenerator.cs

---
+++ src/Shouldly.Tests/ShouldNotContain/PredicateObjectScenario.cs
@@ -0,0 +1,35 @@
+﻿using Shouldly.Tests.TestHelpers;
+using System;
+
+namespace Shouldly.Tests.ShouldNotContain
+{
+    public class PredicateObjectScenario : ShouldlyShouldTestScenario
+    {
+        protected override void ShouldThrowAWobbly()
+        {
+            var a = new Object();
+            var b = new Object();
+            var c = new Object();
+            new[] { a, b, c }.ShouldNotContain(o => o.GetType().FullName.Equals("System.Object"));
+        }
+
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get { return @"
+    new[] { a, b, c }
+        should not contain
+    o.GetType().FullName.Equals(""System.Object"")
+        but does"; }
+            
+        }
+
+        protected override void ShouldPass()
+        {
+            var a = new Object();
+            var b = new Object();
+            var c = new Object();
+            new[] { a, b, c }.ShouldNotContain(o => o.GetType().FullName.Equals(""));
+
+        }
+    }
+}
