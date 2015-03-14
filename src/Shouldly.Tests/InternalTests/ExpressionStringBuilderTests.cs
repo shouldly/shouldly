fd3c4add86870cfecb332cc775604f1548ef52f8
ByteBlast <byteblast@gmail.com>
2015-03-14 21:28:34 +0000

    Added ExpressionStringBuilder

---
+++ src/Shouldly.Tests/InternalTests/ExpressionStringBuilderTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using Shouldly.Internals;
+
+namespace Shouldly.Tests.InternalTests
+{
+    public class ExpressionStringBuilderTests
+    {
+        [Test]
+        public void ToString_ReturnsReadableString()
+        {
+            Expression<Func<int, bool>> func = number => number > 4;
+
+            string actual = ExpressionStringBuilder.ToString(func.Body);
+
+            actual.ShouldBe("(number > 4)");
+        }
+
+        [Test]
+        public void ToString_ClosureOperand_ReturnsReadableString()
+        {
+            var capturedOuterVariable = 4;
+            Expression<Func<int, bool>> func = number => number > capturedOuterVariable;
+
+            var actual = ExpressionStringBuilder.ToString(func.Body);
+
+            actual.ShouldBe("(number > capturedOuterVariable)");
+        }
+    }
+}
