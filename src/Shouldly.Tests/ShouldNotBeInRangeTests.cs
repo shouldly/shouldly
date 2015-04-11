e3f86974d54d3ebf8b51ebae4c1202b3a6ac200a
Jake Ginnivan <jake@ginnivan.net>
2015-04-11 22:06:26 +0100

    Implemented additional info for ShouldBeRangeTestExtensions

--- src/Shouldly.Tests/ShouldNotBeInRangeTests.cs
+++ src/Shouldly.Tests/ShouldNotBeInRangeTests.cs
@@ -1,27 +1,28 @@
 ﻿using NUnit.Framework;
+using Shouldly.Tests.TestHelpers;
 
 namespace Shouldly.Tests
 {
-    public class ShouldNotBeInRangeTests
+    public class ShouldNotBeInRangeTests : ShouldlyShouldTestScenario
     {
-        [Test]
-        public void ShouldThrowWhenInRange()
+        protected override void ShouldThrowAWobbly()
         {
-            Should.Throw<ShouldAssertException>(() => 1.5m.ShouldNotBeInRange(1.4m, 1.6m));
+            1.5m.ShouldNotBeInRange(1.4m, 1.6m, () => "Some additional context");
         }
 
-        [Test]
-        public void ShouldNotThrowWhenOutOfRange()
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get 
+            { 
+                return @"1.5m should not be in range { from = 1.4, to = 1.6 } but was 1.5
+Additional Info:
+Some additional context";
+            }
+        }
+
+        protected override void ShouldPass()
         {
             1.5m.ShouldNotBeInRange(1.6m, 1.7m);
-        }
-
-        [Test]
-        public void ErrorMessageIsNice()
-        {
-            TestHelpers.Should.Error(
-                () => 1.5m.ShouldNotBeInRange(1.4m, 1.6m),
-                "() => 1.5m should not be in range { from = 1.4, to = 1.6 } but was 1.5");
         }
     }
 }
--- src/Shouldly/ShouldlyExtensionMethods/ShouldBeRangeTestExtensions.cs
+++ src/Shouldly/ShouldlyExtensionMethods/ShouldBeRangeTestExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Linq;
 
 namespace Shouldly
@@ -8,24 +7,56 @@ namespace Shouldly
     {
         public static void ShouldBeOneOf<T>(this T actual, params T[] expected)
         {
+            ShouldBeOneOf(actual, expected, () => null);
+        }
+        public static void ShouldBeOneOf<T>(this T actual, T[] expected, string customMessage)
+        {
+            ShouldBeOneOf(actual, expected, () => customMessage);
+        }
+        public static void ShouldBeOneOf<T>(this T actual, T[] expected, Func<string> customMessage)
+        {
             if (!expected.Contains(actual))
-                throw new ShouldAssertException(new ExpectedActualShouldlyMessage(expected, actual).ToString());
+                throw new ShouldAssertException(new ExpectedActualShouldlyMessage(expected, actual, customMessage()).ToString());
         }
 
         public static void ShouldNotBeOneOf<T>(this T actual, params T[] expected)
         {
+            ShouldNotBeOneOf(actual, expected, () => null);
+        }
+        public static void ShouldNotBeOneOf<T>(this T actual, T[] expected, string customMessage)
+        {
+            ShouldNotBeOneOf(actual, expected, () => customMessage);
+        }
+        public static void ShouldNotBeOneOf<T>(this T actual, T[] expected, Func<string> customMessage)
+        {
             if (expected.Contains(actual))
-                throw new ShouldAssertException(new ExpectedActualShouldlyMessage(expected, actual).ToString());
+                throw new ShouldAssertException(new ExpectedActualShouldlyMessage(expected, actual, customMessage()).ToString());
         }
 
         public static void ShouldBeInRange<T>(this T actual, T from, T to) where T : IComparable<T>
         {
-            actual.AssertAwesomely(v => Is.InRange<T>(v, @from, to), actual, new { @from, to });
+            ShouldBeInRange(actual, from, to, () => null);
+        }
+        public static void ShouldBeInRange<T>(this T actual, T from, T to, string customMessage) where T : IComparable<T>
+        {
+            ShouldBeInRange(actual, from, to, () => customMessage);
+        }
+        public static void ShouldBeInRange<T>(this T actual, T from, T to, Func<string> customMessage) where T : IComparable<T>
+        {
+            actual.AssertAwesomely(v => Is.InRange<T>(v, @from, to), actual, new { @from, to }, customMessage);
         }
 
         public static void ShouldNotBeInRange<T>(this T actual, T from, T to) where T : IComparable<T>
         {
-            actual.AssertAwesomely(v => !Is.InRange<T>(v, @from, to), actual, new { @from, to });
+            ShouldNotBeInRange(actual, from, to, () => null);
+        }
+        public static void ShouldNotBeInRange<T>(this T actual, T from, T to, string customMessage) where T : IComparable<T>
+        {
+            ShouldNotBeInRange(actual, from, to, () => customMessage);
+        }
+        public static void ShouldNotBeInRange<T>(this T actual, T from, T to, Func<string> customMessage) where T : IComparable<T>
+        {
+            actual.AssertAwesomely(v => !Is.InRange<T>(v, @from, to), actual, new { @from, to }, customMessage);
         }
     }
 }
