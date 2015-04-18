e1d51b2612526f45f7d06f64052db7849bab24c8
Chaitanya Gurrapu <Chaitanya.Gurrapu@gmail.com>
2015-04-18 23:13:44 +1000

    Interim checkin. Wrote all the unit tests. Got some basic truncation working. Haven't done the multiple diffs yet.

---
+++ src/Shouldly.Tests/Strings/DetailedDifference/LongStrings/LongStringsAreTruncatedAtTheStartAndTheEndBorderConditionTwo.cs
@@ -0,0 +1,46 @@
+﻿using Shouldly.Tests.TestHelpers;
+
+namespace Shouldly.Tests.Strings.DetailedDifference.LongStrings
+{
+    // Just after the edge case for the end of string truncation. Now, both start and end should be truncated.
+    public class LongStringsAreTruncatedAtTheStartBorderConditionBorderConditionTwo : ShouldlyShouldTestScenario
+    {
+        protected override void ShouldPass()
+        {
+            "1A,1b,1c,1d,1e,1f,1g,1h,1i,1j,1k,1l,1m,1n,1o,1p,1q,1r,1s,1t,1u,1v"
+             .ShouldBe(
+            "1a,1b,1c,1d,1e,1f,1g,1h,1i,1j,1k,1l,1m,1n,1o,1p,1q,1r,1s,1t,1u,1v", 
+             Case.Insensitive);
+        }
+
+        protected override void ShouldThrowAWobbly()
+        {
+            "1a,1b,1c,1d,1e,1f,1g,1h,1i,1j,1k,1l,1m,1n,1o,1p,1q,1r,1s,1t,1u,1v"
+             .ShouldBe(
+            "1a,1b,1c,1d,1e,1f,1g,1h,1i,1j,1k,1l,1m,1n,1o,1p,1q,1r.1s,1t,1u,1v", 
+             Case.Insensitive);
+        }
+
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get 
+            {
+                return @"""1a,1b,1c,1d,1e,1f,1g,1h,1i,1j,1k,1l,1m,1n,1o,1p,1q,1r,1s,1t,1u,1v""
+                        should be
+                    ""1a,1b,1c,1d,1e,1f,1g,1h,1i,1j,1k,1l,1m,1n,1o,1p,1q,1r.1s,1t,1u,1v""
+                        but was
+                    ""1a,1b,1c,1d,1e,1f,1g,1h,1i,1j,1k,1l,1m,1n,1o,1p,1q,1r,1s,1t,1u,1v""
+                        difference
+                    Case Insensitive Comparison
+                    Difference     |                                                         |                                                   
+                                   |                                                        \|/                                                  
+                    Index          | ...  43   44   45   46   47   48   49   50   51   52   53   54   55   56   57   58   59   60   61   62   63   ...  
+                    Expected Value | ...  o    ,    1    p    ,    1    q    ,    1    r    .    1    s    ,    1    t    ,    1    u    ,    1    ...  
+                    Actual Value   | ...  o    ,    1    p    ,    1    q    ,    1    r    ,    1    s    ,    1    t    ,    1    u    ,    1    ...  
+                    Expected Code  | ...  111  44   49   112  44   49   113  44   49   114  46   49   115  44   49   116  44   49   117  44   49   ...  
+                    Actual Code    | ...  111  44   49   112  44   49   113  44   49   114  44   49   115  44   49   116  44   49   117  44   49   ...  "
+                    ;
+                }
+        }
+    }
+}
---
+++ src/Shouldly/DifferenceHighlighting/StringDifferenceFormatter.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shouldly.DifferenceHighlighting
+{
+    internal class StringDifferenceFormatter 
+    {
+        private readonly string _actualValue;
+        private readonly string _expectedValue;
+        private readonly Case _caseSensitivity;
+        private readonly int _indexOffset;
+        private StringBuilder differenceStringLineOneBuilder;
+        private StringBuilder actualCodeStringBuilder;
+        private StringBuilder differenceStringLineTwoBuilder;
+        private StringBuilder indexStringBuilder;
+        private StringBuilder expectedValueStringBuilder;
+        private StringBuilder actualValueStringBuilder;
+        private StringBuilder expectedCodeStringBuilder;
+
+        internal bool PrefixWithDots { get; set; }
+        internal bool SuffixWithDots { get; set; }
+
+        internal StringDifferenceFormatter(string actualValue, string expectedValue, Case caseSensitivity, int indexOffset, bool prefixWithDots = false, bool suffixWithDots = false)
+        {
+            _actualValue = actualValue;
+            _expectedValue = expectedValue;
+            _caseSensitivity = caseSensitivity;
+            _indexOffset = indexOffset;
+            PrefixWithDots = prefixWithDots;
+            SuffixWithDots = suffixWithDots;
+
+            differenceStringLineOneBuilder = new StringBuilder();
+            differenceStringLineTwoBuilder = new StringBuilder();
+            indexStringBuilder = new StringBuilder();
+            expectedValueStringBuilder = new StringBuilder();
+            actualValueStringBuilder = new StringBuilder();
+            expectedCodeStringBuilder = new StringBuilder();
+            actualCodeStringBuilder = new StringBuilder();
+        }
+
+        public override string ToString()
+        {
+            return GenerateFormattedString();
+        }
+
+        public string GenerateFormattedString()
+        {
+            int maxLengthOfStrings = Math.Max(_actualValue.Length, _expectedValue.Length);
+            int minLenOfStrings = Math.Min(_actualValue.Length, _expectedValue.Length);
+
+            if (PrefixWithDots)
+            {
+                AddDots();
+            }
+
+            for (int index = 0; index < maxLengthOfStrings; index++)
+            {
+                var isEqual = CheckEquality(index, minLenOfStrings);
+
+                differenceStringLineOneBuilder.Append(string.Format("{0,-5}", isEqual ? " " : @" | " ));
+                differenceStringLineTwoBuilder.Append(string.Format("{0,-5}", isEqual ? " " : @"\|/" ));
+                indexStringBuilder.Append(string.Format("{0,-5}", index + _indexOffset));
+                expectedValueStringBuilder.Append(string.Format("{0,-5}", index < _expectedValue.Length ? _expectedValue[index].ToSafeString() : ""));
+                actualValueStringBuilder.Append(string.Format("{0,-5}", index < _actualValue.Length ? _actualValue[index].ToSafeString() : ""));
+                expectedCodeStringBuilder.Append(string.Format("{0,-5}", index < _expectedValue.Length ? ((int)_expectedValue[index]).ToString() : ""));
+                actualCodeStringBuilder.Append(string.Format("{0,-5}", index < _actualValue.Length ? ((int)_actualValue[index]).ToString() : ""));
+            }
+
+            if (SuffixWithDots)
+            {
+                AddDots();
+            }
+
+            return GetFormattedString();
+        }
+
+        private void AddDots()
+        {
+            differenceStringLineOneBuilder.Append("     ");
+            differenceStringLineTwoBuilder.Append("     ");
+            indexStringBuilder.Append("...  ");
+            expectedValueStringBuilder.Append("...  ");
+            actualValueStringBuilder.Append("...  ");
+            expectedCodeStringBuilder.Append("...  ");
+            actualCodeStringBuilder.Append("...  ");
+        }
+
+        private bool CheckEquality(int index, int minLengthOfStrings)
+        {
+            bool isEqual = false;
+            if (index < minLengthOfStrings)
+            {
+                if (_caseSensitivity == Case.Insensitive)
+                {
+                    isEqual = StringComparer.InvariantCultureIgnoreCase.Equals(_actualValue[index].ToString(), _expectedValue[index].ToString());
+                }
+                else
+                {
+                    isEqual = Equals(_actualValue[index], _expectedValue[index]);
+                }
+            }
+            return isEqual;
+        }
+
+        private string GetFormattedString()
+        {
+            var output = new StringBuilder();
+            output.AppendLine("Difference     | " + differenceStringLineOneBuilder);
+            output.AppendLine("               | " + differenceStringLineTwoBuilder);
+            output.AppendLine("Index          | " + indexStringBuilder);
+            output.AppendLine("Expected Value | " + expectedValueStringBuilder);
+            output.AppendLine("Actual Value   | " + actualValueStringBuilder);
+            output.AppendLine("Expected Code  | " + expectedCodeStringBuilder);
+            output.AppendLine("Actual Code    | " + actualCodeStringBuilder);
+            output.AppendLine();
+
+            var outputString = output.ToString();
+            return outputString;
+        }
+    }
+}
