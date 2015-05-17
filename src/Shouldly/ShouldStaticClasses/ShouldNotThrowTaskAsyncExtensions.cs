746ee1c0160c9d734c5d9435761b3c04dc49048d
yogirajA <yogi.aradhye@hotmail.com>
2015-05-17 11:53:00 -0500

    NotThrowAsync implemented

---
+++ src/Shouldly/ShouldStaticClasses/ShouldNotThrowTaskAsyncExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+
+namespace Shouldly
+{   
+    public static partial class Should
+    {
+      
+        public static Task NotThrowAsync([InstantHandle] Func<Task> action, TimeSpan timeoutAfter,
+            Func<string> customMessage)
+        {
+            return NotThrowAsync(action(),timeoutAfter,customMessage);
+        }
+
+        public static Task NotThrowAsync([InstantHandle] Task task, TimeSpan timeoutAfter,
+            Func<string> customMessage)
+        {
+            try
+            {
+                return task.TimeoutAfter(timeoutAfter);
+            }
+            catch (TimeoutException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw new ShouldAssertException(new ExpectedShouldlyMessage(ex.GetType(), customMessage).ToString());
+            }
+            
+        }
+
+    }
+
+}
