d4c0d058b5ec5033e639fb2fe120f83d37c816c0
Bar Arnon <i3arnon@gmail.com>
2015-05-23 17:54:44 +0300

    Adds Should(Not)BeNullOrWhiteSpace for strings
    Adds ShouldBeNullOrWhiteSpace and ShouldNotBeNullOrWhiteSpace extension
    methods for strings based on String.IsNullOrWhiteSpace

---
+++ src/Shouldly.Tests/Strings/ShouldBeNullOrWhitespace.cs
@@ -0,0 +1,28 @@
+﻿using Shouldly.Tests.TestHelpers;
+
+namespace Shouldly.Tests.Strings
+{
+    public class ShouldBeNullOrWhiteSpace : ShouldlyShouldTestScenario
+    {
+        protected override void ShouldThrowAWobbly()
+        {
+            "a".ShouldBeNullOrWhiteSpace("Some additional context");
+        }
+
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get {
+                return "\"a\" should be null or white space " +
+                       "Additional Info: " +
+                       "Some additional context";
+            }
+        }
+
+        protected override void ShouldPass()
+        {
+            ((string)null).ShouldBeNullOrWhiteSpace();
+            string.Empty.ShouldBeNullOrWhiteSpace();
+            "   ".ShouldBeNullOrWhiteSpace();
+        }
+    }
+}
