b076cb9a993ce3877e26faba29326fee0a6c039e
Bar Arnon <i3arnon@gmail.com>
2015-05-23 17:54:44 +0300

    Adds Should(Not)BeNullOrWhiteSpace for strings
    Adds ShouldBeNullOrWhiteSpace and ShouldNotBeNullOrWhiteSpace extension
    methods for strings based on String.IsNullOrWhiteSpace

---
+++ src/Shouldly/ShouldlyExtensionMethods/ShouldBe/StringNullOrWhitespaceTestExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Shouldly
+{
+    public static partial class ShouldBeStringTestExtensions
+    {
+        public static void ShouldBeNullOrWhiteSpace(this string actual)
+        {
+            ShouldBeNullOrWhiteSpace(actual, () => null);
+        }
+
+        public static void ShouldBeNullOrWhiteSpace(this string actual, string customMessage)
+        {
+            ShouldBeNullOrWhiteSpace(actual, () => customMessage);
+        }
+
+        public static void ShouldBeNullOrWhiteSpace(this string actual, [InstantHandle] Func<string> customMessage)
+        {
+            if (!string.IsNullOrWhiteSpace(actual))
+                throw new ShouldAssertException(new ExpectedShouldlyMessage(actual, customMessage).ToString());
+        }
+
+        public static void ShouldNotBeNullOrWhiteSpace(this string actual)
+        {
+            ShouldNotBeNullOrWhiteSpace(actual, () => null);
+        }
+
+        public static void ShouldNotBeNullOrWhiteSpace(this string actual, string customMessage)
+        {
+            ShouldNotBeNullOrWhiteSpace(actual, () => customMessage);
+        }
+
+        public static void ShouldNotBeNullOrWhiteSpace(this string actual, [InstantHandle] Func<string> customMessage)
+        {
+            if (string.IsNullOrWhiteSpace(actual))
+                throw new ShouldAssertException(new ExpectedShouldlyMessage(actual, customMessage).ToString());
+        }
+    }
+}
