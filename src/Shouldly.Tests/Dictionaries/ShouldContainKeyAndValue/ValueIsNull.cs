ddc3ee00d1e5f195e1d2e9bbff81be7483781a6a
Andy McDowall <andy.mcdowall@ptml.co.nz>
2015-06-18 15:47:21 +1200

    Added failing tests for dictionary value assertions. When a value in the dictionary is null the ShouldContain/ShoukdNotContain KeyAndValue methods will throw NullReferenceExceptions.

---
+++ src/Shouldly.Tests/Dictionaries/ShouldContainKeyAndValue/ValueIsNull.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Shouldly.Tests.TestHelpers;
+
+namespace Shouldly.Tests.Dictionaries.ShouldContainKeyAndValue
+{
+    public class ValueIsNull : ShouldlyShouldTestScenario
+    {
+        readonly Dictionary<MyThing, MyThing> _dictionary = new Dictionary<MyThing, MyThing>
+        {
+            {ThingKey, ThingValue}
+        };
+
+        private static readonly MyThing ThingKey = new MyThing();
+        private static readonly MyThing ThingValue = null;
+
+        protected override void ShouldThrowAWobbly()
+        {
+            _dictionary.ShouldContainKeyAndValue(ThingKey, new MyThing(), "Some additional context");
+        }
+
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get
+            {
+                return
+                    "Dictionary \"_dictionary\" should contain key \"Shouldly.Tests.TestHelpers.MyThing\" with value \"Shouldly.Tests.TestHelpers.MyThing\" but value was null " +
+                    "Additional Info: " +
+                    "Some additional context";
+            }
+        }
+
+        protected override void ShouldPass()
+        {
+            _dictionary.ShouldContainKeyAndValue(ThingKey, ThingValue);
+        }
+    }
+}
