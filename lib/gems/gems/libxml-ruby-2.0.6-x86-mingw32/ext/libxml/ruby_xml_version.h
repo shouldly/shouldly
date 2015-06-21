d495ce7d0c09f12661818162a0de995ce7123f79
Jake Ginnivan <jake@ginnivan.net>
2015-06-21 13:09:53 +0100

    Switching to AppVeyor for builds

--- lib/gems/gems/libxml-ruby-2.0.6-x86-mingw32/ext/libxml/ruby_xml_version.h
+++
@@ -1,9 +0,0 @@
-/* Don't nuke this block!  It is used for automatically updating the
- * versions below. VERSION = string formatting, VERNUM = numbered
- * version for inline testing: increment both or none at all.*/
-#define RUBY_LIBXML_VERSION  "2.0.6"
-#define RUBY_LIBXML_VERNUM   206
-#define RUBY_LIBXML_VER_MAJ   2
-#define RUBY_LIBXML_VER_MIN   0
-#define RUBY_LIBXML_VER_MIC   6
-#define RUBY_LIBXML_VER_PATCH 0
