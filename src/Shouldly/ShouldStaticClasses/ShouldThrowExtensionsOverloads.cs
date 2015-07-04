7d06e506fe38b43c0503bba0bb244876ba7adca2
Jake Ginnivan <jake@ginnivan.net>
2015-07-04 09:17:46 +0800

    Making the ShouldThrow extension methods not exist on the static Should type

--- src/Shouldly/ShouldStaticClasses/ShouldThrowExtensionsOverloads.cs
+++ src/Shouldly/ShouldStaticClasses/ShouldThrowExtensionsOverloads.cs
@@ -1,50 +1,53 @@
 ﻿using System;
+using System.Diagnostics;
 using JetBrains.Annotations;
 
 namespace Shouldly
 {
-    public static partial class Should
+    [DebuggerStepThrough]
+    [ShouldlyMethods]
+    public static class ShouldThrowExtensions
     {
         /*** ShouldThrow(Action) ***/
         public static TException ShouldThrow<TException>(this Action actual) where TException : Exception
         {
-            return Throw<TException>(actual);
+            return Should.Throw<TException>(actual);
         }
         public static TException ShouldThrow<TException>(this Action actual, string customMessage) where TException : Exception
         {
-            return Throw<TException>(actual, customMessage);
+            return Should.Throw<TException>(actual, customMessage);
         }
         public static TException ShouldThrow<TException>(this Action actual, [InstantHandle] Func<string> customMessage) where TException : Exception
         {
-            return Throw<TException>(actual, customMessage);
+            return Should.Throw<TException>(actual, customMessage);
         }
 
         /*** ShouldNotThrow(Action) ***/
         public static void ShouldNotThrow(this Action action)
         {
-            NotThrow(action);
+            Should.NotThrow(action);
         }
         public static void ShouldNotThrow(this Action action, string customMessage)
         {
-            NotThrow(action, customMessage);
+            Should.NotThrow(action, customMessage);
         }
         public static void ShouldNotThrow(this Action action, [InstantHandle] Func<string> customMessage)
         {
-            NotThrow(action, customMessage);
+            Should.NotThrow(action, customMessage);
         }
 
         /*** ShouldNotThrow(Func<T>) ***/
         public static T ShouldNotThrow<T>(this Func<T> action)
         {
-            return NotThrow(action);
+            return Should.NotThrow(action);
         }
         public static T ShouldNotThrow<T>(this Func<T> action, string customMessage)
         {
-            return NotThrow(action, customMessage);
+            return Should.NotThrow(action, customMessage);
         }
         public static T ShouldNotThrow<T>(this Func<T> action, [InstantHandle] Func<string> customMessage)
         {
-            return NotThrow(action, customMessage);
+            return Should.NotThrow(action, customMessage);
         }
     }
 }
