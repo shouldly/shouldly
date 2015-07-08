8229bcc82ca24a4fe6ad88836428c95c408bca96
JoeMighty <josephwoodward@xeuse.com>
2015-07-08 14:19:49 +0100

    Added ShouldBeTrue|False and Null|NotNull extensions

---
+++ src/Shouldly.Tests/ShouldBeNull/NotNullScenario.cs
@@ -0,0 +1,28 @@
+﻿using Shouldly.Tests.TestHelpers;
+
+namespace Shouldly.Tests.ShouldBeNull
+{
+    public class NotNullScenario : ShouldlyShouldTestScenario
+    {
+        protected override void ShouldPass()
+        {
+            "Hello World".ShouldNotBeNull();
+        }
+
+        protected override void ShouldThrowAWobbly()
+        {
+            string myNullRef = null;
+            myNullRef.ShouldNotBeNull("Some additional context");
+        }
+
+        protected override string ChuckedAWobblyErrorMessage
+        {
+            get
+            {
+                return @"myNullRef should not be null but was null
+Additional Info:
+Some additional context";
+            }
+        }
+    }
+}
