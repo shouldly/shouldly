8a72b3abad2aab57b05eb8249b1a3c8cb7d9fb49
Yannis Guedel <me@yannisguedel.ch>
2015-08-12 13:54:17 +0200

    Code is extracted from ShouldNotThrow param
    Added basic functionality to extract the passed parameter to
    ShouldNotThrow when generating the error message.

--- src/Shouldly/MessageGenerators/ShouldNotThrowMessageGenerator.cs
+++ src/Shouldly/MessageGenerators/ShouldNotThrowMessageGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -17,6 +18,21 @@ namespace Shouldly.MessageGenerators
         public override string GenerateErrorMessage(IShouldlyAssertionContext context)
         {
             var codePart = context.CodePart;
+            // When the static method is used instead of the extension method,
+            // the default message generator will extract "Should" as CodePart.
+            if (context.DeterminedOriginatingFrame && context.CodePart == "Should")
+            {
+                var codeLines = string.Join("\n", File.ReadAllLines(context.FileName).Skip(context.LineNumber).ToArray());
+
+                var indexOf = 
+                    codeLines.IndexOf(context.ShouldMethod) +
+                    context.ShouldMethod.Length +
+                    1;
+                var indexOfComma = codeLines.IndexOf(",");
+                if (indexOf > 0 && indexOfComma > 0 && indexOfComma > indexOf)
+                    codePart = codeLines.Substring(indexOf, indexOfComma - indexOf).Trim();
+            }
+
             var expectedValue = context.Expected.ToStringAwesomely();
 
             const string format = @"{0} should not throw but threw {1}";
