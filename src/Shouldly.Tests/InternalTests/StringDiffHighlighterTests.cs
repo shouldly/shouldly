a7add7e8867887918c0b7b3f376aa3e6a9dc995c
sholland <holland_s91@yahoo.com>
2015-10-31 16:13:49 -0500

    Add null checking for diff highlighter params
    Add corresponding tests

---
+++ src/Shouldly.Tests/InternalTests/StringDiffHighlighterTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Shouldly.DifferenceHighlighting2;
+using System;
+
+namespace Shouldly.Tests.InternalTests
+{
+    [TestFixture]
+    public static class StringDiffHighlighterTests
+    {
+        private static IStringDifferenceHighlighter _sut;
+        [SetUp]
+        public static void Setup()
+        {
+            _sut = new StringDifferenceHighlighter(null, 0);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public static void Should_throw_exception_when_expected_arg_is_null()
+        {
+            _sut.HighlightDifferences("not null", null);
+        }
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public static void Should_throw_exception_when_actual_arg_is_null()
+        {
+            _sut.HighlightDifferences(null, "not null");
+        }
+    }
+}
