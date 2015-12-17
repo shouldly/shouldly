0b64e629759ff8e9aca1d49711235d2b77e1b114
josephwoodward <joseph.woodward@landmark.co.uk>
2015-12-17 14:05:29 +0000

    Added additional context

--- src/Shouldly.Tests/Strings/ShouldBeEmpty/ShouldBeEmptyBasicScenario.cs
+++ src/Shouldly.Tests/Strings/ShouldBeEmpty/ShouldBeEmptyBasicScenario.cs
@@ -8,16 +8,22 @@ namespace Shouldly.Tests.Strings.ShouldBeEmpty
         public void ShouldBeEmptyBasicScenarioShouldFail()
         {
             Verify.ShouldFail(() =>
-    "a".ShouldBeEmpty(),
+    "a".ShouldBeEmpty("Some additional context"),
 
     errorWithSource:
     @"""a""
     should be empty but was
-""a""",
+""a""
+
+Additional Info:
+    Some additional context",
 
     errorWithoutSource:
     @"""a""
-    should be empty but was not empty");
+    should be empty but was not empty
+
+Additional Info:
+    Some additional context");
         }
 
         [Fact]
