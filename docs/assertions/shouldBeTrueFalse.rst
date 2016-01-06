f155864d5e409e5c9b95884b8d8be513f88dc91a
Joseph Woodward <josephwoodward@xeuse.com>
2016-01-06 00:26:04 +0000

    Added ShouldBeTrue and ShouldBeFalse docs

---
+++ docs/assertions/shouldBeTrueFalse.rst
@@ -0,0 +1,23 @@
+ShouldBeTrue/False
+========
+
+``ShouldBeTrue`` and ``ShouldBeFalse`` work on boolean values.
+
+ShouldBeTrue
+------------
+.. literalinclude:: /../src/DocumentationExamples/CodeExamples/ShouldBeTrueFalseExamples/ShouldBeTrue.codeSample.approved.txt
+	:language: c#
+
+**Exception**
+
+.. literalinclude:: /../src/DocumentationExamples/CodeExamples/ShouldBeTrueFalseExamples/ShouldBeTrue.exceptionText.approved.txt
+
+ShouldBeFalse
+------------
+
+.. literalinclude:: /../src/DocumentationExamples/CodeExamples/ShouldBeTrueFalseExamples/ShouldBeFalse.codeSample.approved.txt
+	:language: c#
+
+**Exception**
+
+.. literalinclude:: /../src/DocumentationExamples/CodeExamples/ShouldBeTrueFalseExamples/ShouldBeFalse.exceptionText.approved.txt
---
+++ src/DocumentationExamples/ShouldBeTrueFalseExamples.cs
@@ -0,0 +1,37 @@
+﻿using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DocumentationExamples
+{
+    public class ShouldBeTrueFalseExamples
+    {
+        readonly ITestOutputHelper _testOutputHelper;
+
+        public ShouldBeTrueFalseExamples(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void ShouldBeTrue()
+        {
+            DocExampleWriter.Document(() =>
+            {
+                bool myValue = false;
+                myValue.ShouldBeTrue();
+            }, _testOutputHelper);
+        }
+
+        [Fact]
+        public void ShouldBeFalse()
+        {
+            DocExampleWriter.Document(() =>
+            {
+                bool myValue = true;
+                myValue.ShouldBeFalse();
+            }, _testOutputHelper);
+        }
+
+    }
+}
