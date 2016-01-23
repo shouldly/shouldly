d3006076757adc03d6689aff5c7637a5e08af684
Jesper Bangsholt <jkb@guldmann.com>
2016-01-23 01:59:32 +0100

    Added Message Generators

---
+++ src/Shouldly.Shared/MessageGenerators/ShouldHaveFlagMessageGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Shouldly.MessageGenerators
+{
+    internal class ShouldHaveFlagMessageGenerator : ShouldlyMessageGenerator
+    {
+        public override bool CanProcess(IShouldlyAssertionContext context)
+        {
+            return context.ShouldMethod == "ShouldHaveFlag";
+        }
+
+        public override string GenerateErrorMessage(IShouldlyAssertionContext context)
+        {
+            var codePart = context.CodePart;
+            var expectedValue = context.Expected.ToStringAwesomely();
+            
+            var actual = context.Actual.ToStringAwesomely();
+            var actualString = codePart == actual ? " did not" : $@" had
+{actual}";
+
+            return $@"{codePart}
+    should have flag
+{expectedValue}
+    but{actualString}";
+        }
+    }
+}
---
+++ src/Shouldly.Shared/MessageGenerators/ShouldNotHaveFlagMessageGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shouldly.MessageGenerators
+{
+    internal class ShouldNotHaveFlagMessageGenerator : ShouldlyMessageGenerator
+    {
+        public override bool CanProcess(IShouldlyAssertionContext context)
+        {
+            return context.ShouldMethod == "ShouldNotHaveFlag";
+        }
+
+        public override string GenerateErrorMessage(IShouldlyAssertionContext context)
+        {
+            var codePart = context.CodePart;
+            var expectedValue = context.Expected.ToStringAwesomely();
+
+            var actual = context.Actual.ToStringAwesomely();
+            var actualString = codePart == actual ? " had" : $@" it had
+{actual}";
+
+            return $@"{codePart}
+    should not have flag
+{expectedValue}
+    but{actualString}";
+        }
+    }
+}
