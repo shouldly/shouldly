87a57ce771d48a600595509b5c2bc20552645f69
Jesper Bangsholt <jkb@guldmann.com>
2016-01-23 02:01:00 +0100

    Added tests

---
+++ src/Shouldly.Tests.Shared/ShouldHaveFlag/ShouldHaveFlagScenario.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shouldly.ShouldlyExtensionMethods;
+using Shouldly.Tests.Strings;
+using Xunit;
+
+namespace Shouldly.Tests.Shared.ShouldHaveFlag
+{
+    public class ShouldHaveFlagScenario
+    {
+        [Fact]
+        public void FlagScenarioShouldFail()
+        {
+            var actual = TestEnum.FlagTwo;
+            var value = TestEnum.FlagOne;
+
+            Verify.ShouldFail(() => actual.ShouldHaveFlag(value, "Some additional context"), 
+                errorWithSource: 
+@"Verify
+    should have flag
+TestEnum.FlagOne
+    but had
+TestEnum.FlagTwo
+
+Additional Info:
+    Some additional context", 
+                errorWithoutSource:
+@"TestEnum.FlagTwo
+    should have flag
+TestEnum.FlagOne
+    but did not
+
+Additional Info:
+    Some additional context");
+        }
+        
+        [Fact]
+        public void ShouldPassOneFlagSet()
+        {
+            var actual = TestEnum.FlagOne;
+            var value = TestEnum.FlagOne;
+
+            actual.ShouldHaveFlag(value);
+        }
+
+        [Fact]
+        public void ShouldPassTwoFlagsSet()
+        {
+            var actual = TestEnum.FlagOne | TestEnum.FlagTwo;
+            var value = TestEnum.FlagTwo;
+
+            actual.ShouldHaveFlag(value);
+        }
+    }
+
+    [Flags]
+    public enum TestEnum
+    {
+        FlagOne = 1,
+        FlagTwo = 2,
+        FlagThree = 4
+    }
+}
---
+++ src/Shouldly.Tests.Shared/ShouldNotHaveFlag/ShouldNotHaveFlagScenarios.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shouldly.ShouldlyExtensionMethods;
+using Shouldly.Tests.Strings;
+using Xunit;
+
+namespace Shouldly.Tests.Shared.ShouldNotHaveFlag
+{
+    public class ShouldNotHaveFlagScenarios
+    {
+        [Fact]
+        public void FlagScenarioShouldFail()
+        {
+            var actual = TestEnum.FlagOne;
+            var value = TestEnum.FlagOne;
+
+            Verify.ShouldFail(() => actual.ShouldNotHaveFlag(value, "Some additional context"),
+                errorWithSource:
+@"Verify
+    should not have flag
+TestEnum.FlagOne
+    but it had
+TestEnum.FlagOne
+
+Additional Info:
+    Some additional context",
+                errorWithoutSource:
+@"TestEnum.FlagOne
+    should not have flag
+TestEnum.FlagOne
+    but had
+
+Additional Info:
+    Some additional context");
+        }
+
+        [Fact]
+        public void ShouldPassOneFlagSet()
+        {
+            var actual = TestEnum.FlagOne;
+            var value = TestEnum.FlagTwo;
+
+            actual.ShouldNotHaveFlag(value);
+        }
+
+        [Fact]
+        public void ShouldPassTwoFlagsSet()
+        {
+            var actual = TestEnum.FlagOne | TestEnum.FlagTwo;
+            var value = TestEnum.FlagThree;
+
+            actual.ShouldNotHaveFlag(value);
+        }
+    }
+
+    [Flags]
+    public enum TestEnum
+    {
+        FlagOne = 1,
+        FlagTwo = 2,
+        FlagThree = 4
+    }
+}
