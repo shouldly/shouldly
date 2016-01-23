0649cac0827c1d296ec8e508f4803fc96d3198e3
Jake Ginnivan <jake@ginnivan.net>
2016-01-23 11:54:24 +0800

    Fixed tests

--- src/Shouldly.Shared/ShouldlyExtensionMethods/ShouldBeEnumExtensions.cs
+++ src/Shouldly.Shared/ShouldlyExtensionMethods/ShouldBeEnumExtensions.cs
@@ -9,20 +9,16 @@ namespace Shouldly.ShouldlyExtensionMethods
 {
     public static class ShouldHaveEnumExtensions
     {
-        public static void ShouldHaveFlag(this Enum actual, Enum expectedFlag) 
+        public static void ShouldHaveFlag(this Enum actual, Enum expectedFlag)
             => ShouldHaveFlag(actual, expectedFlag, () => null);
 
-        public static void ShouldHaveFlag(this Enum actual, Enum expectedFlag, string customMessage) 
+        public static void ShouldHaveFlag(this Enum actual, Enum expectedFlag, string customMessage)
             => ShouldHaveFlag(actual, expectedFlag, () => customMessage);
-        
+
         public static void ShouldHaveFlag(this Enum actual, Enum expectedFlag, [InstantHandle] Func<string> customMessage)
         {
             CheckEnumHasFlagAttribute(actual);
-#if net35
             if (!actual.HasFlag(expectedFlag))
-#elif net40
-            if (!actual.HasFlag(expectedFlag))
-#endif
             {
                 throw new ShouldAssertException(new ExpectedActualShouldlyMessage(expectedFlag, actual, customMessage).ToString());
             }
@@ -38,14 +34,10 @@ namespace Shouldly.ShouldlyExtensionMethods
             [InstantHandle] Func<string> customMessage)
         {
             CheckEnumHasFlagAttribute(actual);
-#if net35
             if (actual.HasFlag(expectedFlag))
-#elif net40
-            if (actual.HasFlag(expectedFlag))
-#endif
             {
                 throw new ShouldAssertException(new ExpectedActualShouldlyMessage(expectedFlag, actual, customMessage).ToString());
-            }    
+            }
         }
 
         static void CheckEnumHasFlagAttribute(Enum actual)
@@ -63,7 +55,7 @@ namespace Shouldly.ShouldlyExtensionMethods
 #if net35
         /* If the .NET Framework doesn't have a HasFlag, patch in our own version. 
             Made by decompiling the HasFlag function on enum in .NET Framework version 4.5.1 */
-        private static bool HasFlag(this Enum enumeration, Enum value)
+        static bool HasFlag(this Enum enumeration, Enum value)
         {
             if (enumeration == null)
             {
@@ -80,7 +72,7 @@ namespace Shouldly.ShouldlyExtensionMethods
                 throw new ArgumentException($"Enumeration type mismatch. The flag is of type '{value.GetType()}', was expecting {enumeration.GetType()}");
             }
 
-            ulong longValue = Convert.ToUInt64(value);
+            var longValue = Convert.ToUInt64(value);
 
             return (Convert.ToUInt64(enumeration) & longValue) == longValue;
         }
