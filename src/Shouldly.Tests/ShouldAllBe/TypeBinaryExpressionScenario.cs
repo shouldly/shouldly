84c90506a1c2b6777e2303a2c5e34e16702063f2
ssiko <StevenTSikorski@gmail.com>
2016-07-29 00:05:56 -0400

    Adding VisitTypeBinary override for ExpressionStringBuilder

---
+++ src/Shouldly.Tests/ShouldAllBe/TypeBinaryExpressionScenario.cs
@@ -0,0 +1,46 @@
+﻿using Shouldly.Tests.Strings;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Shouldly.Tests.ShouldAllBe
+{       
+    public class TypeBinaryExpressionScenario
+    {
+        [Fact]
+        public void TypeBinaryExpressionScenarioShouldFail()
+        {
+            List<object> objects = new List<object> { "1", 1 };
+                        
+            Verify.ShouldFail(() => 
+objects.ShouldAllBe(x => x is string, "Some additional context"),
+
+errorWithSource:
+@"objects
+    should satisfy the condition
+(x Is String)
+    but
+[1]
+    do not
+
+Additional Info:
+    Some additional context",
+
+errorWithoutSource:
+@"[""1"", 1]
+    should satisfy the condition
+(x Is String)
+    but
+[1]
+    do not
+
+Additional Info:
+    Some additional context");
+        }
+
+        [Fact]
+        public void ShouldPass()
+        {
+            new[] { "1", "2", "3" }.ShouldAllBe(x => x is string);
+        }
+    }
+}
