7955d690cfd929d8df96132779e6484a8b465c58
RJ Hollberg <rj.hollberg@outlook.com>
2017-07-24 11:59:35 -0600

    Define public API.

---
+++ src/Shouldly/ShouldlyExtensionMethods/ShouldBe/ObjectGraphTestExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Shouldly
+{
+    [ShouldlyMethods]
+    public static class ObjectGraphTestExtensions
+    {
+        public static void ShouldBeEquivalentTo(this object actual, object expected)
+        {
+            ShouldBeEquivalentTo(actual, expected, () => null);
+        }
+
+        public static void ShouldBeEquivalentTo(this object actual, object expected, string customMessage)
+        {
+            ShouldBeEquivalentTo(actual, expected, () => customMessage);
+        }
+
+        public static void ShouldBeEquivalentTo(this object actual, object expected, [InstantHandle] Func<string> customMessage)
+        {
+        }
+    }
+}
