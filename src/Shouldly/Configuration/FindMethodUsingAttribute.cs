8e25a70cc9765b411e45889a9f2a8f38af31b4bb
Dennis Roche <dennisroche@outlook.com>
2017-08-01 23:41:52 +0800

    Added GetTypeInfo in order to access GetCustomAttributes

--- src/Shouldly/Configuration/FindMethodUsingAttribute.cs
+++ src/Shouldly/Configuration/FindMethodUsingAttribute.cs
@@ -1,6 +1,7 @@
 #if ShouldMatchApproved
 using System;
 using System.Diagnostics;
+using System.Reflection;
 using System.Linq;
 
 namespace Shouldly.Configuration
@@ -18,8 +19,11 @@ namespace Shouldly.Configuration
                     throw new Exception($"Cannot find method in call stack with attribute {typeof(T).FullName}");
                 }
                 callingFrame = stackTrace.GetFrame(i++);
+#if NETSTANDARD2_0
+            } while (!callingFrame.GetMethod().GetType().GetTypeInfo().GetCustomAttributes(typeof(T), true).Any());
+#else
             } while (!callingFrame.GetMethod().GetCustomAttributes(typeof(T), true).Any());
-
+#endif
             return new TestMethodInfo(callingFrame);
         }
     }
