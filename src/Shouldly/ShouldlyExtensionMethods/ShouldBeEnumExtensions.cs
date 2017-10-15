d03fcc3329ea32cb48318a41d3b8045f92bbae8a
Joseph Woodward <joseph.woodward@xeuse.com>
2017-10-16 00:20:06 +0100

    Tests working

--- src/Shouldly/ShouldlyExtensionMethods/ShouldBeEnumExtensions.cs
+++ src/Shouldly/ShouldlyExtensionMethods/ShouldBeEnumExtensions.cs
@@ -4,7 +4,6 @@ using JetBrains.Annotations;
 
 namespace Shouldly.ShouldlyExtensionMethods
 {
-    [ShouldlyMethods]
     public static class ShouldHaveEnumExtensions
     {
         public static void ShouldHaveFlag(this Enum actual, Enum expectedFlag)
