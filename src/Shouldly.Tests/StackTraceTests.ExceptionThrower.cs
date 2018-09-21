b3acc898058ec5692462809a6086a5b98b2f3419
Joseph Musser <me@jnm2.com>
2018-09-21 18:01:12 -0400

    Removed irrelevant stack frames from exception stack traces (#501)
    * Failing tests for relevant stack trace
    
    * Removed irrelevant stack frames from exceptions
    
    * Failing tests for matching trailing whitespace of default stack trace
    
    * Match trailing whitespace of default exception stack trace

---
+++ src/Shouldly.Tests/StackTraceTests.ExceptionThrower.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Shouldly.Tests
+{
+    partial class StackTraceTests
+    {
+        public sealed class ExceptionThrower
+        {
+            public Type ExceptionType { get; }
+            public bool InShouldlyAssembly { get; }
+            public Action ThrowingAction { get; }
+
+            public ExceptionThrower(Type exceptionType, bool inShouldlyAssembly, Action throwingAction)
+            {
+                ExceptionType = exceptionType ?? throw new ArgumentNullException(nameof(exceptionType));
+                InShouldlyAssembly = inShouldlyAssembly;
+                ThrowingAction = throwingAction ?? throw new ArgumentNullException(nameof(throwingAction));
+            }
+
+            public override string ToString()
+            {
+                return InShouldlyAssembly ? ThrowingAction.Method.Name :
+                    ExceptionType.Name + " thrown directly";
+            }
+
+            public Exception Catch()
+            {
+                // Don’t rely on a framework for this in case of the outside chance that the framework manipulates the stack trace.
+                try
+                {
+                    ThrowingAction.Invoke();
+                    return null;
+                }
+                catch (Exception ex) when (ex.GetType() == ExceptionType)
+                {
+                    return ex;
+                }
+            }
+        }
+    }
+}
