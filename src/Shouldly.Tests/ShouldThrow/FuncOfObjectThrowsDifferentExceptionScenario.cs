c4f13004f7c361900b43c94740c5e130fc3b1f5f
martincostello <martin@martincostello.com>
2018-10-07 13:55:14 +0100

    Add Func-based Should.Throw methods
    Inspired by xunit's Assert.Throws(), add Func-based Should.Throw() methods to remove the need to create dummy variables for actions and to remove the need to suppress warnings from tools like FxCop for unused objects.

---
+++ src/Shouldly.Tests/ShouldThrow/FuncOfObjectThrowsDifferentExceptionScenario.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Shouldly.Tests.Strings;
+using Xunit;
+
+namespace Shouldly.Tests.ShouldThrow
+{
+    public class FuncOfObjectThrowsDifferentExceptionScenario
+    {
+        [Fact]
+        public void NestedBlockLambdaWithoutAdditionalInformationsScenarioShouldFail()
+        {
+            Func<object> action = () => { throw new RankException(); };
+            Verify.ShouldFail(() =>
+action.ShouldThrow<InvalidOperationException>("Some additional context"),
+
+errorWithSource:
+@"`action()`
+    should throw
+System.InvalidOperationException
+    but threw
+System.RankException
+
+Additional Info:
+    Some additional context",
+
+errorWithoutSource:
+@"delegate
+    should throw
+System.InvalidOperationException
+    but threw
+System.RankException
+
+Additional Info:
+    Some additional context");
+        }    
+
+    [Fact]
+    public void NestedBlockLambdaWithoutAdditionalInformationsScenarioShouldFail_ExceptionTypePassedIn()
+    {
+        Func<object> action = () => { throw new RankException(); };
+        Verify.ShouldFail(() =>
+action.ShouldThrow("Some additional context", typeof(InvalidOperationException)),
+
+errorWithSource:
+@"`action()`
+    should throw
+System.InvalidOperationException
+    but threw
+System.RankException
+
+Additional Info:
+    Some additional context",
+
+errorWithoutSource:
+@"delegate
+    should throw
+System.InvalidOperationException
+    but threw
+System.RankException
+
+Additional Info:
+    Some additional context");
+    }
+}
+}
