c4aa6f50f87084df92160f0335aeaf9e53b51bda
Joseph Musser <me@jnm2.com>
2018-11-06 15:07:47 -0500

    Fixed null string comparison (#538)
    * Failing tests that difference should not be shown for missing string
    
    * Stop showing misleading difference for missing string

---
+++ src/Shouldly.Tests/Strings/DetailedDifference/CaseSensitive/NullScenario.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+
+namespace Shouldly.Tests.Strings.DetailedDifference.CaseSensitive
+{
+    public static class NullScenario
+    {
+        [Fact]
+        public static void ShouldNotShowDifferenceWhenActualIsMissing()
+        {
+            var str = (string)null;
+            Verify.ShouldFail(() =>
+str.ShouldBe("null"),
+
+errorWithSource:
+@"str
+    should be
+""null""
+    but was
+null",
+
+errorWithoutSource:
+@"null
+    should be
+""null""
+    but was not");
+        }
+
+        [Fact]
+        public static void ShouldNotShowDifferenceWhenExpectedIsMissing()
+        {
+            var str = "null";
+            Verify.ShouldFail(() =>
+str.ShouldBe(null),
+
+errorWithSource:
+@"str
+    should be
+null
+    but was
+""null""",
+
+errorWithoutSource:
+@"""null""
+    should be
+null
+    but was not");
+        }
+    }
+}
