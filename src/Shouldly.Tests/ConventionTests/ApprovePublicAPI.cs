da7692bd9ded8d6c2f3165dcb4133fccf31281d3
Joseph Woodward <josephwoodward@xeuse.com>
2018-11-21 08:49:05 +0000

    cold hands

--- src/Shouldly.Tests/ConventionTests/ApprovePublicAPI.cs
+++ src/Shouldly.Tests/ConventionTests/ApprovePublicAPI.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using Shouldly.Configuration;
 using Xunit;
 using Xunit.Abstractions;
@@ -17,9 +18,28 @@ namespace Shouldly.Tests.ConventionTests
         public void ShouldlyApi()
         {
             _output.WriteLine("Joe - Hello World!");
-            var res = new DoNotLaunchWhenEnvVariableIsPresent("AppVeyor");
-            var result = res.ShouldNotLaunch();
-            _output.WriteLine("Joe ShouldNotLaunch: " + result);
+
+            string env = "AppVeyor";
+            
+            var res1 = Environment.GetEnvironmentVariable(env);
+            _output.WriteLine("res1 = " + res1);
+            
+            var res2 =  Environment.GetEnvironmentVariable(env, EnvironmentVariableTarget.User);
+            _output.WriteLine("res2 = " + res2);
+            
+            var res3 = Environment.GetEnvironmentVariable(env, EnvironmentVariableTarget.Machine);
+            _output.WriteLine("res3 = " + res3);
+            
+            string env2 = "APPVEYOR";
+            
+            var res4 = Environment.GetEnvironmentVariable(env2);
+            _output.WriteLine("res4 = " + res4);
+            
+            var res5 =  Environment.GetEnvironmentVariable(env2, EnvironmentVariableTarget.User);
+            _output.WriteLine("res5 = " + res5);
+            
+            var res6 = Environment.GetEnvironmentVariable(env2, EnvironmentVariableTarget.Machine);
+            _output.WriteLine("res6 = " + res6);
             
             var publicApi = PublicApiGenerator.ApiGenerator.GeneratePublicApi(typeof(Should).Assembly);
             publicApi.ShouldMatchApproved(b => b.WithFileExtension("cs"));
