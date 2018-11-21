c8cad0436f87328227590a08917b0af473cfdd14
Joseph Woodward <josephwoodward@xeuse.com>
2018-11-21 17:08:10 +0000

    Filename fixed

---
+++ src/Shouldly.Tests/ConventionTests/ApprovePublicApi.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace Shouldly.Tests.ConventionTests
+{
+    public class ApprovePublicApi
+    {
+        [Fact]
+        public void ShouldlyApi()
+        {
+            var publicApi = PublicApiGenerator.ApiGenerator.GeneratePublicApi(typeof(Should).Assembly);
+            publicApi.ShouldMatchApproved(b => b.WithFileExtension("cs"));
+        }  
+    }
+}
