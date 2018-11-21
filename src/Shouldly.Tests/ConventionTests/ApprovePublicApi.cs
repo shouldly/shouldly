6d14ae95bfcb12a7a0a816b1ae32e667bb77a7ca
Joseph Woodward <josephwoodward@xeuse.com>
2018-11-21 17:21:11 +0000

    Added temp mac test

--- src/Shouldly.Tests/ConventionTests/ApprovePublicApi.cs
+++ src/Shouldly.Tests/ConventionTests/ApprovePublicApi.cs
@@ -4,11 +4,20 @@ namespace Shouldly.Tests.ConventionTests
 {
     public class ApprovePublicApi
     {
+        #if IsWindows
         [Fact]
-        public void ShouldlyApi()
+        public void ShouldlyApiWindows()
         {
             var publicApi = PublicApiGenerator.ApiGenerator.GeneratePublicApi(typeof(Should).Assembly);
             publicApi.ShouldMatchApproved(b => b.WithFileExtension("cs"));
-        }  
+        }
+        #else
+        [Fact]
+        public void ShouldlyApiMac()
+        {
+            var publicApi = PublicApiGenerator.ApiGenerator.GeneratePublicApi(typeof(Should).Assembly);
+            publicApi.ShouldMatchApproved(b => b.WithFileExtension("cs"));
+        }
+        #endif
     }
 }
