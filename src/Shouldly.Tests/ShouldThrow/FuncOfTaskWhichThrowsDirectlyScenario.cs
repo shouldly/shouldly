fb5899946f0fa62690b4738f98003a35817c7919
jnm2 <me@jnm2.com>
2019-10-13 19:50:22 -0400

    Failing test for NRE and incorrect exception message when AggregateException has no inner exceptions

--- src/Shouldly.Tests/ShouldThrow/FuncOfTaskWhichThrowsDirectlyScenario.cs
+++ src/Shouldly.Tests/ShouldThrow/FuncOfTaskWhichThrowsDirectlyScenario.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Shouldly.Tests.Strings;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -36,19 +38,53 @@ namespace Shouldly.Tests.ShouldThrow
             var task = new Func<Task>(() => { throw new TimeoutException(); });
             task.ShouldThrow(typeof(TimeoutException));
         }
-        
+
         [Fact]
         public void ShouldPassTimeoutExceptionAsync()
         {
             var task = new Func<Task>(async () => { throw new TimeoutException(); });
             task.ShouldThrow<TimeoutException>();
         }
-       
+
         [Fact]
         public void ShouldPassTimeoutExceptionAsync_ExceptionTypePassedIn()
         {
             var task = new Func<Task>(async () => { throw new TimeoutException(); });
             task.ShouldThrow(typeof(TimeoutException));
+        }
+
+        [Theory]
+        [InlineData(false, false)]
+        [InlineData(false, true)]
+        [InlineData(true, false)]
+        [InlineData(true, true)]
+        public void ShouldHandleAggregateExceptionWithNoInnerExceptions(bool withSynchronizationContext, bool useGenericOverload)
+        {
+            var func = new Func<Task>(() => throw new AggregateException());
+
+            if (!withSynchronizationContext)
+                SynchronizationContext.SetSynchronizationContext(null);
+
+            Verify.ShouldFail(
+                () =>
+                {
+                    if (useGenericOverload)
+                        func.ShouldThrow<InvalidOperationException>();
+                    else
+                        func.ShouldThrow(typeof(InvalidOperationException));
+                },
+                errorWithSource:
+@"Task `func`
+    should throw
+System.InvalidOperationException
+    but threw
+System.AggregateException",
+                errorWithoutSource:
+@"Task
+    should throw
+System.InvalidOperationException
+    but threw
+System.AggregateException");
         }
     }
 }
