b19ee1d15e51887f23cd951543f58d7e42325a11
jnm2 <me@jnm2.com>
2019-10-16 18:42:00 -0400

    Let GetFrame decide whether `i` is out of bounds since it returns null

--- src/Shouldly/Configuration/FindMethodUsingAttribute.cs
+++ src/Shouldly/Configuration/FindMethodUsingAttribute.cs
@@ -13,11 +13,9 @@ namespace Shouldly.Configuration
             StackFrame callingFrame;
             do
             {
-                if (i >= stackTrace.FrameCount)
-                {
-                    throw new Exception($"Cannot find method in call stack with attribute {typeof(T).FullName}");
-                }
-                callingFrame = stackTrace.GetFrame(i++);
+                callingFrame = stackTrace.GetFrame(i++)
+                    ?? throw new Exception($"Cannot find method in call stack with attribute {typeof(T).FullName}.");
+
             } while (!callingFrame.GetMethod().GetCustomAttributes(typeof(T), true).Any());
 
             return new TestMethodInfo(callingFrame);
