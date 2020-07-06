0ed7bf91efa5a39c16230fb29b7e7a3c5c6b8f26
SimonCropp <simon.cropp@gmail.com>
2020-07-06 15:18:36 +1000

    Update TestMethodInfo.cs

--- src/Shouldly/Configuration/TestMethodInfo.cs
+++ src/Shouldly/Configuration/TestMethodInfo.cs
@@ -53,9 +53,7 @@ namespace Shouldly.Configuration
 
         static bool ContainsAttribute(object[] attributes, string attributeName)
         {
-            {
-                return attributes.Any(a => a.GetType().FullName.StartsWith(attributeName));
-            }
+            return attributes.Any(a => a.GetType().FullName.StartsWith(attributeName));
         }
 
         public string SourceFileDirectory { get; private set; }
