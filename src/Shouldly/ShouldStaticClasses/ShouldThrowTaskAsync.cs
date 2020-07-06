7bbd2446e3b7591865df283c66b2a8e3b9846262
SimonCropp <simon.cropp@gmail.com>
2020-07-06 19:15:36 +1000

    Update ShouldThrowTaskAsync.cs

--- src/Shouldly/ShouldStaticClasses/ShouldThrowTaskAsync.cs
+++ src/Shouldly/ShouldStaticClasses/ShouldThrowTaskAsync.cs
@@ -9,15 +9,20 @@ namespace Shouldly
     public static partial class Should
     {
         /*** Should.ThrowAsync(Task) ***/
-        public static Task<TException> ThrowAsync<TException>(Task task) where TException : Exception
+        public static Task<TException> ThrowAsync<TException>(Task task)
+            where TException : Exception
         {
             return ThrowAsync<TException>(task, () => null);
         }
-        public static Task<TException> ThrowAsync<TException>(Task task, string? customMessage) where TException : Exception
+
+        public static Task<TException> ThrowAsync<TException>(Task task, string? customMessage)
+            where TException : Exception
         {
             return ThrowAsync<TException>(task, () => customMessage);
         }
-        public static Task<TException> ThrowAsync<TException>(Task task, [InstantHandle] Func<string?>? customMessage) where TException : Exception
+
+        public static Task<TException> ThrowAsync<TException>(Task task, [InstantHandle] Func<string?>? customMessage)
+            where TException : Exception
         {
             return ThrowAsync<TException>(() => task, customMessage);
         }
@@ -26,26 +31,32 @@ namespace Shouldly
         {
             return ThrowAsync(task, () => null, exceptionType);
         }
+
         public static Task<Exception> ThrowAsync(Task task, string? customMessage, Type exceptionType)
         {
             return ThrowAsync(task, () => customMessage, exceptionType);
         }
+
         public static Task<Exception> ThrowAsync(Task task, [InstantHandle] Func<string?>? customMessage, Type exceptionType)
         {
             return ThrowAsync(() => task, customMessage, exceptionType);
         }
 
         /*** Should.ThrowAsync(Func<Task>) ***/
-        public static Task<TException> ThrowAsync<TException>(Func<Task> actual) where TException : Exception
+        public static Task<TException> ThrowAsync<TException>(Func<Task> actual)
+            where TException : Exception
         {
             return ThrowAsync<TException>(actual, () => null);
         }
-        public static Task<TException> ThrowAsync<TException>(Func<Task> actual, string? customMessage) where TException : Exception
+
+        public static Task<TException> ThrowAsync<TException>(Func<Task> actual, string? customMessage)
+            where TException : Exception
         {
             return ThrowAsync<TException>(actual, () => customMessage);
         }
 
-        public static Task<TException> ThrowAsync<TException>(Func<Task> actual, [InstantHandle] Func<string?>? customMessage) where TException : Exception
+        public static Task<TException> ThrowAsync<TException>(Func<Task> actual, [InstantHandle] Func<string?>? customMessage)
+            where TException : Exception
         {
 #if StackTrace
             var stackTrace = new StackTrace(true);
@@ -53,65 +64,65 @@ namespace Shouldly
             {
                 return actual()
                     .ContinueWith(new Func<Task, Exception>(t =>
+                    {
+                        if (t.IsFaulted)
                         {
-                            if (t.IsFaulted)
-                            {
-                                if (t.Exception!.InnerException is TException expectedException)
-                                    return expectedException;
+                            if (t.Exception!.InnerException is TException expectedException)
+                                return expectedException;
 
-                                // If Task.IsFaulted is true, there is at least one inner exception.
-                                return new ShouldAssertException(new AsyncShouldlyThrowShouldlyMessage(typeof(TException), t.Exception.InnerException!.GetType(), customMessage, stackTrace).ToString());
-                            }
+                            // If Task.IsFaulted is true, there is at least one inner exception.
+                            return new ShouldAssertException(new AsyncShouldlyThrowShouldlyMessage(typeof(TException), t.Exception.InnerException!.GetType(), customMessage, stackTrace).ToString());
+                        }
 
-                            if (t.IsCanceled)
-                            {
-                                return new TaskCanceledException(t);
-                            }
+                        if (t.IsCanceled)
+                        {
+                            return new TaskCanceledException(t);
+                        }
 
-                            return new ShouldAssertException(new AsyncShouldlyThrowShouldlyMessage(typeof(TException), customMessage, stackTrace).ToString());
-                        }))
+                        return new ShouldAssertException(new AsyncShouldlyThrowShouldlyMessage(typeof(TException), customMessage, stackTrace).ToString());
+                    }))
                     .ContinueWith(x =>
+                    {
+                        switch (x.Result)
                         {
-                            switch (x.Result)
-                            {
-                                case TException expectedException:
-                                    return expectedException;
-                                case ShouldAssertException assert:
-                                    throw assert;
-                                default:
-                                    throw new ShouldAssertException(new AsyncShouldlyThrowShouldlyMessage(typeof(TException), x.Result.GetType(), customMessage, stackTrace).ToString(), x.Result);
-                            }
-                        });
-                }
-                catch (Exception e)
-                {
-                    if (e is TException)
-                        return Task.FromResult((TException)e);
+                            case TException expectedException:
+                                return expectedException;
+                            case ShouldAssertException assert:
+                                throw assert;
+                            default:
+                                throw new ShouldAssertException(new AsyncShouldlyThrowShouldlyMessage(typeof(TException), x.Result.GetType(), customMessage, stackTrace).ToString(), x.Result);
+                        }
+                    });
+            }
+            catch (Exception e)
+            {
+                if (e is TException exception)
+                    return Task.FromResult(exception);
 
-                    throw new ShouldAssertException(new AsyncShouldlyThrowShouldlyMessage(typeof(TException), e.GetType(), customMessage, stackTrace).ToString());
-                }
+                throw new ShouldAssertException(new AsyncShouldlyThrowShouldlyMessage(typeof(TException), e.GetType(), customMessage, stackTrace).ToString());
+            }
 #else
             try
             {
                 return actual()
                     .ContinueWith(new Func<Task, Exception>(t =>
+                    {
+                        if (t.IsFaulted)
                         {
-                            if (t.IsFaulted)
-                            {
-                                if (t.Exception!.InnerException is TException expectedException)
-                                    return expectedException;
+                            if (t.Exception!.InnerException is TException expectedException)
+                                return expectedException;
 
-                                // If Task.IsFaulted is true, there is at least one inner exception.
-                                return new ShouldAssertException(new TaskShouldlyThrowMessage(typeof(TException), t.Exception.InnerException!.GetType(), customMessage).ToString());
-                            }
+                            // If Task.IsFaulted is true, there is at least one inner exception.
+                            return new ShouldAssertException(new TaskShouldlyThrowMessage(typeof(TException), t.Exception.InnerException!.GetType(), customMessage).ToString());
+                        }
 
-                            if (t.IsCanceled)
-                            {
-                                return new TaskCanceledException(t);
-                            }
+                        if (t.IsCanceled)
+                        {
+                            return new TaskCanceledException(t);
+                        }
 
-                            return new ShouldAssertException(new TaskShouldlyThrowMessage(typeof(TException), customMessage).ToString());
-                        }))
+                        return new ShouldAssertException(new TaskShouldlyThrowMessage(typeof(TException), customMessage).ToString());
+                    }))
                     .ContinueWith(x =>
                     {
                         switch (x.Result)
@@ -130,7 +141,7 @@ namespace Shouldly
             catch (Exception e)
             {
                 if (e is TException)
-                    return Task.FromResult((TException)e);
+                    return Task.FromResult((TException) e);
 
                 throw new ShouldAssertException(new TaskShouldlyThrowMessage(typeof(TException), e.GetType(), customMessage).ToString());
             }
@@ -142,10 +153,12 @@ namespace Shouldly
         {
             return ThrowAsync(actual, () => null, exceptionType);
         }
+
         public static Task<Exception> ThrowAsync(Func<Task> actual, string? customMessage, Type exceptionType)
         {
             return ThrowAsync(actual, () => customMessage, exceptionType);
         }
+
         public static Task<Exception> ThrowAsync(Func<Task> actual, [InstantHandle] Func<string?>? customMessage, Type exceptionType)
         {
 #if StackTrace
@@ -191,10 +204,12 @@ namespace Shouldly
         {
             return NotThrowAsync(task, () => null);
         }
+
         public static Task NotThrowAsync(Task task, string? customMessage)
         {
             return NotThrowAsync(task, () => customMessage);
         }
+
         public static Task NotThrowAsync(Task task, [InstantHandle] Func<string?>? customMessage)
         {
             return NotThrowAsync(() => task, customMessage);
@@ -205,10 +220,12 @@ namespace Shouldly
         {
             return NotThrowAsync(actual, () => null);
         }
+
         public static Task NotThrowAsync(Func<Task> actual, string? customMessage)
         {
             return NotThrowAsync(actual, () => customMessage);
         }
+
         public static Task NotThrowAsync(Func<Task> actual, [InstantHandle] Func<string?>? customMessage)
         {
             return NotThrowAsyncInternal(actual, customMessage);
@@ -226,7 +243,7 @@ namespace Shouldly
                 if (t.IsFaulted)
                 {
                     var flattened = t.Exception!.Flatten();
-                    if (flattened.InnerExceptions.Count == 1 && flattened.InnerException!=null)
+                    if (flattened.InnerExceptions.Count == 1 && flattened.InnerException != null)
                     {
                         var inner = flattened.InnerException;
                         throw new ShouldAssertException(new AsyncShouldlyNotThrowShouldlyMessage(inner.GetType(), customMessage, stackTrace, inner.Message, shouldlyMethod).ToString());
@@ -242,7 +259,7 @@ namespace Shouldly
                 if (t.IsFaulted)
                 {
                     var flattened = t.Exception!.Flatten();
-                    if (flattened.InnerExceptions.Count == 1 && flattened.InnerException!=null)
+                    if (flattened.InnerExceptions.Count == 1 && flattened.InnerException != null)
                     {
                         var inner = flattened.InnerException;
                         throw new ShouldAssertException(new TaskShouldlyThrowMessage(inner.GetType(), inner.Message, customMessage, shouldlyMethod).ToString());
