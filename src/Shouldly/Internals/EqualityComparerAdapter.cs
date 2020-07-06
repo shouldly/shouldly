66480e1271e95c4f8f221d60acc14437623ef9f3
SimonCropp <simon.cropp@gmail.com>
2020-07-06 15:32:57 +1000

    Update EqualityComparerAdapter.cs

--- src/Shouldly/Internals/EqualityComparerAdapter.cs
+++ src/Shouldly/Internals/EqualityComparerAdapter.cs
@@ -4,11 +4,10 @@ using System.Collections.Generic;
 
 namespace Shouldly
 {
-    /* 
+    /*
      * Code heavily influenced by code from xunit assert equality comparer adapter
      * at https://github.com/xunit/xunit/blob/master/src/xunit2.assert/Asserts/Sdk/AssertEqualityComparerAdapter.cs
      */
-
     internal class EqualityComparerAdapter<T> : IEqualityComparer
     {
         private readonly IEqualityComparer<T> _innerComparer;
