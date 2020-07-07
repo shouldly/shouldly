9bb376a1543bc2e4457ca96a5585bddfddea8423
Simon Cropp <simon.cropp@gmail.com>
2020-07-07 23:03:07 +1000

    .

--- src/Shouldly/ShouldlyExtensionMethods/ShouldBeEnumerableTestExtensions.cs
+++ src/Shouldly/ShouldlyExtensionMethods/ShouldBeEnumerableTestExtensions.cs
@@ -51,96 +51,56 @@ namespace Shouldly
 
         public static void ShouldContain<T>(this IEnumerable<T> actual, [InstantHandle] Expression<Func<T, bool>> elementPredicate, string? customMessage = null)
         {
-            ShouldContain(actual, elementPredicate, () => customMessage);
-        }
-
-        public static void ShouldContain<T>(this IEnumerable<T> actual, [InstantHandle] Expression<Func<T, bool>> elementPredicate, [InstantHandle] Func<string?>? customMessage)
-        {
             var condition = elementPredicate.Compile();
             if (!actual.Any(condition))
-                throw new ShouldAssertException(new ExpectedActualShouldlyMessage(elementPredicate.Body, actual, customMessage).ToString());
+                throw new ShouldAssertException(new ExpectedActualShouldlyMessage(elementPredicate.Body, actual, () => customMessage).ToString());
         }
 
         public static void ShouldNotContain<T>(this IEnumerable<T> actual, [InstantHandle] Expression<Func<T, bool>> elementPredicate, string? customMessage = null)
         {
-            ShouldNotContain(actual, elementPredicate, () => customMessage);
-        }
-
-        public static void ShouldNotContain<T>(this IEnumerable<T> actual, [InstantHandle] Expression<Func<T, bool>> elementPredicate, [InstantHandle] Func<string?>? customMessage)
-        {
             var condition = elementPredicate.Compile();
             if (actual.Any(condition))
-                throw new ShouldAssertException(new ExpectedActualShouldlyMessage(elementPredicate.Body, actual, customMessage).ToString());
+                throw new ShouldAssertException(new ExpectedActualShouldlyMessage(elementPredicate.Body, actual, () => customMessage).ToString());
         }
 
         public static void ShouldAllBe<T>(this IEnumerable<T> actual, [InstantHandle] Expression<Func<T, bool>> elementPredicate, string? customMessage = null)
         {
-            ShouldAllBe(actual, elementPredicate, () => customMessage);
-        }
-
-        public static void ShouldAllBe<T>(this IEnumerable<T> actual, [InstantHandle] Expression<Func<T, bool>> elementPredicate, [InstantHandle] Func<string?>? customMessage)
-        {
             var condition = elementPredicate.Compile();
             var actualResults = actual.Where(part => !condition(part));
             if (actualResults.Any())
-                throw new ShouldAssertException(new ActualFilteredWithPredicateShouldlyMessage(elementPredicate.Body, actualResults, actual, customMessage).ToString());
+                throw new ShouldAssertException(new ActualFilteredWithPredicateShouldlyMessage(elementPredicate.Body, actualResults, actual, () => customMessage).ToString());
         }
 
         public static void ShouldBeEmpty<T>([NotNull] this IEnumerable<T>? actual, string? customMessage = null)
         {
-            ShouldBeEmpty(actual, () => customMessage);
-        }
-
-        public static void ShouldBeEmpty<T>([NotNull] this IEnumerable<T>? actual, [InstantHandle] Func<string?>? customMessage)
-        {
             if (actual == null || actual.Any())
-                throw new ShouldAssertException(new ExpectedShouldlyMessage(actual, customMessage).ToString());
+                throw new ShouldAssertException(new ExpectedShouldlyMessage(actual, () => customMessage).ToString());
         }
 
         public static void ShouldNotBeEmpty<T>([NotNull] this IEnumerable<T>? actual, string? customMessage = null)
         {
-            ShouldNotBeEmpty(actual, () => customMessage);
-        }
-
-        public static void ShouldNotBeEmpty<T>([NotNull] this IEnumerable<T>? actual, [InstantHandle] Func<string?>? customMessage)
-        {
             if (actual == null || !actual.Any())
-                throw new ShouldAssertException(new ExpectedShouldlyMessage(actual, customMessage).ToString());
+                throw new ShouldAssertException(new ExpectedShouldlyMessage(actual, () => customMessage).ToString());
         }
 
         public static T ShouldHaveSingleItem<T>([NotNull] this IEnumerable<T>? actual, string? customMessage = null)
         {
-            return ShouldHaveSingleItem(actual, () => customMessage);
-        }
-
-        public static T ShouldHaveSingleItem<T>([NotNull] this IEnumerable<T>? actual, [InstantHandle] Func<string?>? customMessage)
-        {
             if (actual == null || actual.Count() != 1)
-                throw new ShouldAssertException(new ExpectedShouldlyMessage(actual, customMessage).ToString());
+                throw new ShouldAssertException(new ExpectedShouldlyMessage(actual, () => customMessage).ToString());
 
             return actual.Single();
         }
 
         public static void ShouldContain(this IEnumerable<float> actual, float expected, double tolerance, string? customMessage = null)
         {
-            ShouldContain(actual, expected, tolerance, () => customMessage);
-        }
-
-        public static void ShouldContain(this IEnumerable<float> actual, float expected, double tolerance, [InstantHandle] Func<string?>? customMessage)
-        {
             if (!actual.Any(a => Math.Abs(expected - a) < tolerance))
-                throw new ShouldAssertException(new ExpectedActualToleranceShouldlyMessage(expected, actual, tolerance, customMessage).ToString());
+                throw new ShouldAssertException(new ExpectedActualToleranceShouldlyMessage(expected, actual, tolerance, () => customMessage).ToString());
         }
 
         public static void ShouldContain(this IEnumerable<double> actual, double expected, double tolerance, string? customMessage = null)
         {
-            ShouldContain(actual, expected, tolerance, () => customMessage);
-        }
-
-        public static void ShouldContain(this IEnumerable<double> actual, double expected, double tolerance, [InstantHandle] Func<string?>? customMessage)
-        {
             if (!actual.Any(a => Math.Abs(expected - a) < tolerance))
-                throw new ShouldAssertException(new ExpectedActualToleranceShouldlyMessage(expected, actual, tolerance, customMessage).ToString());
+                throw new ShouldAssertException(new ExpectedActualToleranceShouldlyMessage(expected, actual, tolerance, () => customMessage).ToString());
         }
 
         public static void ShouldBeSubsetOf<T>(this IEnumerable<T> actual, IEnumerable<T> expected, string? customMessage = null)
@@ -160,29 +120,19 @@ namespace Shouldly
 
         public static void ShouldBeUnique<T>(this IEnumerable<T> actual, string? customMessage = null)
         {
-            ShouldBeUnique(actual, () => customMessage);
-        }
-
-        public static void ShouldBeUnique<T>(this IEnumerable<T> actual, [InstantHandle] Func<string?>? customMessage)
-        {
             var duplicates = GetDuplicates(actual);
             if (duplicates.Any())
-                throw new ShouldAssertException(new ExpectedActualShouldlyMessage(actual, duplicates, customMessage).ToString());
+                throw new ShouldAssertException(new ExpectedActualShouldlyMessage(actual, duplicates, () => customMessage).ToString());
         }
 
         public static void ShouldBe(this IEnumerable<string> actual, IEnumerable<string> expected, Case caseSensitivity, string? customMessage = null)
-        {
-            ShouldBe(actual, expected, caseSensitivity, () => customMessage);
-        }
-
-        public static void ShouldBe(this IEnumerable<string> actual, IEnumerable<string> expected, Case caseSensitivity, [InstantHandle] Func<string?>? customMessage)
         {
             actual.AssertAwesomelyWithCaseSensitivity(
                 v => Is.EnumerableStringEqualWithCaseSensitivity(v, expected, caseSensitivity),
                 actual,
                 expected,
                 caseSensitivity,
-                customMessage);
+                () => customMessage);
         }
 
         public static void ShouldBeInOrder<T>(this IEnumerable<T> actual, string? customMessage = null)
