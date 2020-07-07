80fd9a459dd4282797fc0154c925b55135a29615
Simon Cropp <simon.cropp@gmail.com>
2020-07-07 23:48:21 +1000

    Update ShouldCompleteInExtensions.cs

--- src/Shouldly/ShouldStaticClasses/ShouldCompleteInExtensions.cs
+++ src/Shouldly/ShouldStaticClasses/ShouldCompleteInExtensions.cs
@@ -13,7 +13,7 @@ namespace Shouldly
         {
             var actual = Task.Factory.StartNew(action, CancellationToken.None, TaskCreationOptions.None,
                         TaskScheduler.Default);
-            CompleteIn(actual, timeout, () => customMessage, "Delegate");
+            CompleteIn(actual, timeout, customMessage, "Delegate");
         }
 
         /*** CompleteIn(Func<T>) ***/
@@ -21,41 +21,38 @@ namespace Shouldly
         {
             var actual = Task.Factory.StartNew(function, CancellationToken.None, TaskCreationOptions.None,
                         TaskScheduler.Default);
-
-            return CompleteIn(actual, timeout, () => customMessage, "Delegate");
+            CompleteIn(actual, timeout, customMessage, "Delegate");
+            return actual.Result;
         }
 
         /*** CompleteIn(Func<Task>) ***/
         public static void CompleteIn(Func<Task> actual, TimeSpan timeout, string? customMessage = null)
         {
-            CompleteIn(actual(), timeout, () => customMessage, "Task");
+            CompleteIn(actual(), timeout, customMessage, "Task");
         }
 
         /*** CompleteIn(Func<Task<T>>) ***/
         public static T CompleteIn<T>(Func<Task<T>> actual, TimeSpan timeout, string? customMessage = null)
         {
-            return CompleteIn(actual(), timeout, () => customMessage, "Task");
+            Task<T> task = actual();
+            CompleteIn(task, timeout, customMessage, "Task");
+            return task.Result;
         }
 
         /*** CompleteIn(Task<T>) ***/
         public static void CompleteIn(Task actual, TimeSpan timeout, string? customMessage = null)
         {
-            CompleteIn(actual, timeout, () => customMessage, "Task");
+            CompleteIn(actual, timeout, customMessage, "Task");
         }
 
         /*** CompleteIn(Task<T>) ***/
         public static T CompleteIn<T>(Task<T> actual, TimeSpan timeout, string? customMessage = null)
         {
-            return CompleteIn(actual, timeout, () => customMessage, "Task");
-        }
-
-        private static T CompleteIn<T>(Task<T> actual, TimeSpan timeout, [InstantHandle] Func<string?>? customMessage, string what)
-        {
-            CompleteIn((Task)actual, timeout, customMessage, what);
+            CompleteIn((Task)actual, timeout, customMessage, "Task");
             return actual.Result;
         }
 
-        private static void CompleteIn(Task actual, TimeSpan timeout, [InstantHandle] Func<string?>? customMessage, string what)
+        private static void CompleteIn(Task actual, TimeSpan timeout, string? customMessage, string what)
         {
             try
             {
@@ -71,7 +68,7 @@ namespace Shouldly
                 // When exception is a timeout exception we can provide a better error, otherwise rethrow
                 if (inner is ShouldlyTimeoutException exception)
                 {
-                    var message = new CompleteInShouldlyMessage(what, timeout, customMessage).ToString();
+                    var message = new CompleteInShouldlyMessage(what, timeout, () => customMessage).ToString();
                     throw new ShouldCompleteInException(message, exception);
                 }
 
