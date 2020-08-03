4bcee501287c8cb5ca00faf0f96e667805e73914
Nathan Keir <kermitt@bigpond.net.au>
2020-08-03 10:47:05 +1000

    Modify FindMethodUsingAttribute to discover async test methods.

--- src/Shouldly/Configuration/FindMethodUsingAttribute.cs
+++ src/Shouldly/Configuration/FindMethodUsingAttribute.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 
 namespace Shouldly.Configuration
 {
@@ -9,13 +11,59 @@ namespace Shouldly.Configuration
         {
             for (var i = startAt; stackTrace.GetFrame(i) is { } frame; i++)
             {
-                if (frame.GetMethod() is { } method && method.IsDefined(typeof(T), inherit: true))
+                var method = frame.GetMethod();
+                var originalMethod = GetOriginalMethodInfoForStateMachineMethod(method);
+                method = originalMethod != null ? originalMethod.Value.DeclaringType.GetMethod(originalMethod.Value.MethodName) : method;
+
+                if ((method?.IsDefined(typeof(T), inherit: true)).GetValueOrDefault())
                 {
                     return new TestMethodInfo(frame);
                 }
             }
 
             throw new InvalidOperationException($"Cannot find a method in the stack trace with attribute {typeof(T).FullName}.");
+        }
+
+        private static OriginalMethodInfo? GetOriginalMethodInfoForStateMachineMethod(MethodBase? method)
+        {
+            if (method?.DeclaringType is { IsByRef: false } declaringType
+                && declaringType.DeclaringType is { } originalMethodDeclaringType
+                && ContainsAttribute(declaringType, "System.Runtime.CompilerServices.CompilerGeneratedAttribute")
+                && declaringType.GetInterface("System.Runtime.CompilerServices.IAsyncStateMachine") is object)
+            {
+                var stateMachineTypeName = declaringType.Name;
+                var openingAngleBracket = stateMachineTypeName.IndexOf('<');
+                if (openingAngleBracket != -1)
+                {
+                    var closingAngleBracket = stateMachineTypeName.IndexOf('>', openingAngleBracket + 1);
+                    if (closingAngleBracket != -1)
+                    {
+                        var originalMethodName = stateMachineTypeName.Substring(openingAngleBracket + 1, closingAngleBracket - (openingAngleBracket + 1));
+
+                        return new OriginalMethodInfo(originalMethodName, originalMethodDeclaringType);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static bool ContainsAttribute(MemberInfo member, string attributeName)
+        {
+            return member.CustomAttributes.Any(a =>
+                a.AttributeType.FullName?.StartsWith(attributeName, StringComparison.Ordinal) ?? false);
+        }
+
+        private readonly struct OriginalMethodInfo
+        {
+            public OriginalMethodInfo(string methodName, Type declaringType)
+            {
+                MethodName = methodName;
+                DeclaringType = declaringType;
+            }
+
+            public string MethodName { get; }
+            public Type DeclaringType { get; }
         }
     }
 }
