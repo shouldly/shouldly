632173b234abe9276d31c93eaa70cc11ca238a68
jswolf19 <jswolf19@hotmail.com>
2021-01-10 20:52:42 +0900

    Added tests for retraversing Enumerables

---
+++ src/Shouldly.Tests/ShouldBe/EnumerableType/NonRetraversableEnumerable.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Shouldly.Tests.Strings;
+using Xunit;
+
+namespace Shouldly.Tests.ShouldBe.EnumerableType
+{
+    public class NonRetraversableEnumerable
+    {
+        private static IEnumerable<int> CreateTestEnumerable()
+        {
+            var foo = new TestEnumerable(new[] {
+                new[] {1, 2, 3 },
+                new[] {4, 5, 6 },
+                new[] {7, 8, 9 } });
+
+            return foo.ReadLine();
+        }
+
+        [Fact]
+        public void ActualEnumerableShouldOnlyBeTraversedOnce()
+        {
+            var fooEnum = CreateTestEnumerable();
+
+            Verify.ShouldFail(() =>
+                fooEnum.ShouldBe(new [] {3, 2, 1 }),
+                errorWithSource:
+@"fooEnum
+    should be
+[3, 2, 1]
+    but was
+[1, 2, 3]
+    difference
+[*1*, 2, *3*]",
+                errorWithoutSource:
+@"[1, 2, 3]
+    should be
+[3, 2, 1]
+    but was not
+    difference
+[*1*, 2, *3*]");
+        }
+
+        [Fact]
+        public void ExpectedEnumerableShouldOnlyBeTraversedOnce()
+        {
+            var fooEnum = CreateTestEnumerable();
+
+            Verify.ShouldFail(() =>
+                new[] { 3, 2, 1 }.ShouldBe(fooEnum),
+                errorWithSource:
+@"new[] { 3, 2, 1 }
+    should be
+[1, 2, 3]
+    but was
+[3, 2, 1]
+    difference
+[*3*, 2, *1*]",
+                errorWithoutSource:
+@"[3, 2, 1]
+    should be
+[1, 2, 3]
+    but was not
+    difference
+[*3*, 2, *1*]");
+        }
+
+        [Fact]
+        public void TestEnumerableIsNotRetraversable()
+        {
+            var fooEnum = CreateTestEnumerable();
+
+            fooEnum.ShouldSatisfyAllConditions(
+                () => fooEnum.ShouldBe(new[] { 1, 2, 3 }),
+                () => fooEnum.First().ShouldBe(4),
+                () => fooEnum.First().ShouldBe(5),
+                () => fooEnum.First().ShouldBe(6),
+                () => fooEnum.Any().ShouldBeFalse(),
+                () => fooEnum.First().ShouldBe(7),
+                () => fooEnum.ShouldBe(new[] {8, 9 }));
+        }
+
+        private class TestEnumerable
+        {
+            private readonly int[][] _baseData;
+            private int _curLine;
+            private int _curCol;
+
+            public TestEnumerable(int[][] baseData)
+            {
+                _baseData = baseData;
+                _curLine = 0;
+                _curCol = -1;
+            }
+
+            public IEnumerable<int> ReadLine()
+            {
+                if(_curLine < _baseData.Length)
+                {
+                    _curCol++;
+                    while (_curCol < _baseData[_curLine].Length)
+                    {
+                        yield return _baseData[_curLine][_curCol];
+                        _curCol++;
+                    }
+                    _curLine++;
+                    _curCol = -1;
+                }
+            }
+        }
+    }
+}
