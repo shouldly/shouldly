17f649ebf660ab5e25d3f4d647655f8817759fc7
jnm2 <me@jnm2.com>
2021-03-28 22:04:38 -0400

    SA0001 XML comment analysis is disabled due to project configuration

--- src/Shouldly/Internals/ShouldlyCoreExtensions.cs
+++ src/Shouldly/Internals/ShouldlyCoreExtensions.cs
@@ -17,12 +17,12 @@ namespace Shouldly
         }
 
         /// <summary>
-        /// Required to support the <see cref="DynamicShould.HaveProperty"/> method that takes in a <see
-        /// langword="dynamic"/> as a parameter. Having a method that takes a dynamic really stuffs up the stack trace
-        /// because the runtime binder has to inject a whole heap of methods. Our normal way of just taking the next
-        /// frame doesn't work. The following two lines seem to work for now, but this feels like a hack. The conditions
-        /// to be able to walk up stack trace until we get to the calling method might have to be updated regularly as
-        /// we find more scenarios. Alternately, it could be replaced with a more robust implementation.
+        /// Required to support the <see cref="DynamicShould.HaveProperty(dynamic, string, string?)"/> method that takes
+        /// in a <see langword="dynamic"/> as a parameter. Having a method that takes a dynamic really stuffs up the
+        /// stack trace because the runtime binder has to inject a whole heap of methods. Our normal way of just taking
+        /// the next frame doesn't work. The following two lines seem to work for now, but this feels like a hack. The
+        /// conditions to be able to walk up stack trace until we get to the calling method might have to be updated
+        /// regularly as we find more scenarios. Alternately, it could be replaced with a more robust implementation.
         /// </summary>
         internal static bool IsSystemDynamicMachinery(this MethodBase method)
         {
