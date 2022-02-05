50e51b5ae1078aa0496544b8745f7f32ade3859d
spencerr <spencerritzow@gmail.com>
2022-02-05 11:08:59 -0600

    Update ShouldBeDictionaryTestExtensions.cs
    Use IEnumerable instead of ICollection for readonly..

--- src/Shouldly/ShouldlyExtensionMethods/ShouldBeDictionaryTestExtensions.cs
+++ src/Shouldly/ShouldlyExtensionMethods/ShouldBeDictionaryTestExtensions.cs
@@ -7,7 +7,7 @@ namespace Shouldly
     [ShouldlyMethods]
     public static partial class ShouldBeDictionaryTestExtensions
     {
-        public static void ShouldContainKey<TKey, TValue>(this ICollection<KeyValuePair<TKey, TValue>> dictionary, TKey key, string? customMessage = null)
+        public static void ShouldContainKey<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> dictionary, TKey key, string? customMessage = null)
             where TKey : notnull
         {
             var containsKey = dictionary switch
@@ -21,21 +21,21 @@ namespace Shouldly
                 throw new ShouldAssertException(new ExpectedActualShouldlyMessage(key, dictionary, customMessage).ToString());
         }
 
-        public static void ShouldNotContainKey<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, string? customMessage = null)
+        public static void ShouldNotContainKey<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> dictionary, TKey key, string? customMessage = null)
             where TKey : notnull
         {
             if (dictionary.ContainsKey(key))
                 throw new ShouldAssertException(new ExpectedActualShouldlyMessage(key, dictionary, customMessage).ToString());
         }
 
-        public static void ShouldContainKeyAndValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue val, string? customMessage = null)
+        public static void ShouldContainKeyAndValue<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> dictionary, TKey key, TValue val, string? customMessage = null)
             where TKey : notnull
         {
             if (!dictionary.ContainsKey(key) || !Equals(dictionary[key], val))
                 throw new ShouldAssertException(new ExpectedActualKeyShouldlyMessage(val, dictionary, key, customMessage).ToString());
         }
 
-        public static void ShouldNotContainValueForKey<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue val, string? customMessage = null)
+        public static void ShouldNotContainValueForKey<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> dictionary, TKey key, TValue val, string? customMessage = null)
             where TKey : notnull
         {
             if (!dictionary.ContainsKey(key) || Equals(dictionary[key], val))
