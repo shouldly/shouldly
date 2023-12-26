2bacedf99fde356c03458c9728d8ce7df324b66f
Michal Cocuľa <michal.cocula@student.tuke.sk>
2023-12-26 18:33:02 +0100

    Fixing comment.

---
+++ src/Shouldly.Tests/ShouldBe/ImageScenarios.cs
@@ -0,0 +1,102 @@
+﻿namespace Shouldly.Tests.ShouldBe;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1416:...", Justification = "Explanation for suppression.")]
+public class ImageScenarios
+{
+    [Fact]
+    public void ImageBytes2x2ShouldBeEqual()
+    {
+        byte[] expectedImage = LoadImage("PNG_2x2_RRBW.png");
+        byte[] actualImage = (byte[])expectedImage.Clone();
+        expectedImage.ShouldBe(actualImage);
+    }
+
+    [Fact]
+    public void ImageBytes2x2ShouldNotBeEqual()
+    {
+        byte[] expectedImage = LoadImage("PNG_2x2_RRBW.png");
+        byte[] actualImage = LoadImage("PNG_2x2_WWWW.png");
+        Should.Throw<ShouldAssertException>(
+            () => expectedImage.ShouldBe(actualImage))
+            .Message.ShouldBe(
+             """
+expectedImage
+    should be
+[[255, 255, 255, 255], [255, 255, 255, 255], [255, 255, 255, 255], [255, 255, 255, 255]]
+    but was
+[[255, 255, 0, 0], [255, 255, 0, 0], [255, 0, 0, 255], [255, 255, 255, 255]]
+    difference
+[*[255, 255, 0, 0]*, *[255, 255, 0, 0]*, *[255, 0, 0, 255]*, [255, 255, 255, 255]]
+"""
+);
+    }
+
+    [Fact]
+    public void ImageBytesDifferentSizeShouldNotBeEqual()
+    {
+
+        byte[] expectedImage = LoadImage("PNG_2x3_WWWWBB.png");
+        byte[] actualImage = LoadImage("PNG_3x2_WWWWBB.png");
+        Should.Throw<ShouldAssertException>(
+            () => expectedImage.ShouldBe(actualImage))
+            .Message.ShouldBe(
+             """
+expectedImage dimensions [Width x Height] should be [3 x 2] but were [2 x 3]
+"""
+);
+    }
+
+    [Fact]
+    public void ImageBytesDifferentFormatShouldNotBeEqual()
+    {
+        byte[] expectedImage = LoadImage("PNG_2x3_WWWWBB.png");
+        byte[] actualImage = LoadImage("JPEG_2x3_WWWWBB.jpeg");
+        Should.Throw<ShouldAssertException>(
+            () => expectedImage.ShouldBe(actualImage))
+            .Message.ShouldBe(
+             """
+expectedImage image format should be JPEG but was PNG
+"""
+);
+    }
+
+    [Fact]
+    public void RegularBytesThrowMessageTest()
+    {
+        byte[] expected = { 1, 1, 0, 1, 0, 0, 0, 0 };
+        byte[] actual = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        Should.Throw<ShouldAssertException>(
+            () => expected.ShouldBe(actual))
+            .Message.ShouldBe(
+            """
+expected
+    should be
+[0, 0, 0, 0, 0, 0, 0, 0, 0]
+    but was
+[1, 1, 0, 1, 0, 0, 0, 0]
+    difference
+[*1*, *1*, 0, *1*, 0, 0, 0, 0, *]
+"""
+);
+    }
+
+
+
+
+    private static byte[] LoadImage(string fileName)
+    {
+
+        string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "assets", fileName);
+        try
+        {
+            byte[] content = File.ReadAllBytes(path);
+            return content;
+        }
+        catch (IOException ex)
+        {
+            Assert.Fail(ex.Message);
+            return null;
+        }
+    }
+
+}
--- src/Shouldly/MessageGenerators/ImageShouldBeEqualGenerator.cs
+++ src/Shouldly/MessageGenerators/ImageShouldBeEqualGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace Shouldly.MessageGenerators;
 
@@ -28,7 +29,8 @@ class ImageShouldBeEqualGenerator : ShouldlyMessageGenerator
 
         if (!expectedBitmap.RawFormat.Equals(actualBitmap.RawFormat))
         {
-            return $@"{context.CodePart} image format {context.ShouldMethod.PascalToSpaced()} {expectedBitmap.RawFormat} but was {actualBitmap.RawFormat}";
+
+            return $@"{context.CodePart} image format {context.ShouldMethod.PascalToSpaced()} {GetImageName(expectedBitmap.RawFormat)} but was {GetImageName(actualBitmap.RawFormat)}";
         }
 
         if (!expectedBitmap.Width.Equals(actualBitmap.Width) || !expectedBitmap.Height.Equals(actualBitmap.Height))
@@ -48,7 +50,7 @@ class ImageShouldBeEqualGenerator : ShouldlyMessageGenerator
             using MemoryStream stream = new MemoryStream(imageBytes);
             Bitmap bitmap = new Bitmap(stream);
             return bitmap;
-            
+
         }
         catch (ArgumentException)
         {
@@ -58,7 +60,7 @@ class ImageShouldBeEqualGenerator : ShouldlyMessageGenerator
         {
             throw new Exception(e.Message);
         }
-       
+
     }
 
     private static byte[][] ExtractPixels(Bitmap bitmap)
@@ -66,7 +68,7 @@ class ImageShouldBeEqualGenerator : ShouldlyMessageGenerator
         int width = bitmap.Width;
         int height = bitmap.Height;
 
-        byte[][] pixelData = new byte[height*width][];
+        byte[][] pixelData = new byte[height * width][];
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
@@ -79,10 +81,24 @@ class ImageShouldBeEqualGenerator : ShouldlyMessageGenerator
                     Array.Reverse(convertedPixel);
                 }
 
-                pixelData[y*width+x] = convertedPixel;
+                pixelData[y * width + x] = convertedPixel;
             }
         }
         return pixelData;
+    }
+
+    private static string GetImageName(ImageFormat format)
+    {
+        if (format == ImageFormat.Jpeg) return "JPEG";
+        if (format == ImageFormat.Bmp) return "BMP";
+        if (format == ImageFormat.Png) return "PNG";
+        if (format == ImageFormat.Emf) return "EMF";
+        if (format == ImageFormat.Exif) return "EXIF";
+        if (format == ImageFormat.Gif) return "GIF";
+        if (format == ImageFormat.Icon) return "ICON";
+        if (format == ImageFormat.MemoryBmp) return "MEMORY_BMP";
+        if (format == ImageFormat.Tiff) return "TIFF";
+        return "Wmf";
     }
 }
 
