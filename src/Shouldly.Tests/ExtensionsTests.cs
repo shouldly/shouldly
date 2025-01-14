47f288c6cfb55ebdd267dd6a9dd7113f680f3aa1
Wenzel, Toni <toni.wenzel@chg-meridian.com>
2025-01-14 22:38:55 +0100

    Provide simple As extension

---
+++ src/Shouldly.Tests/ExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Shouldly.Tests;
+
+public class ExtensionsTests
+{
+    [Fact]
+    public void Should_Cast_To_Object()
+    {
+        Should.NotThrow(
+            () => 1.As<object>());
+    }
+
+    [Fact]
+    public void Should_Cast_To_Type()
+    {
+        object person = new MyThing();
+        Should.NotThrow(
+            () => person.As<MyThing>().ShouldNotBeNull());
+    }
+
+    [Fact]
+    public void Should_Return_Null()
+    {
+        object person = new MyThing();
+        Should.NotThrow(
+            () => person.As<MyDecoratedThing>().ShouldBeNull());
+    }
+}
---
+++ src/Shouldly/Extensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Shouldly;
+
+/// <summary>
+/// Common extensions
+/// </summary>
+public static class Extensions
+{
+    /// <summary>
+    /// Casts the subject to the specified type if possible, otherwise returns null.
+    /// </summary>
+    /// <typeparam name="TTo">The type to cast the subject to.</typeparam>
+    /// <param name="subject">The subject to be casted.</param>
+    public static TTo? As<TTo>(this object? subject)
+    {
+        return subject is TTo to ? to : default;
+    }
+}
