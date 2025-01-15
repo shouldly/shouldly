eca358d05ee2fb0a4c69c47539908750a38e22fe
Shane Krueger <shane@acdmail.com>
2025-01-15 16:45:03 -0500

    Add NRT annotation to ShouldBeAssignableTo (#933)
    * Update ShouldBeTypeTestExtensions.cs
    
    * Api approval
    
    * Update

--- src/Shouldly/ShouldlyExtensionMethods/ShouldBeTypeTestExtensions.cs
+++ src/Shouldly/ShouldlyExtensionMethods/ShouldBeTypeTestExtensions.cs
@@ -4,6 +4,7 @@ namespace Shouldly;
 
 public static partial class ShouldBeTestExtensions
 {
+    [return: NotNullIfNotNull("actual")] 
     public static T? ShouldBeAssignableTo<T>(this object? actual, string? customMessage = null)
     {
         ShouldBeAssignableTo(actual, typeof(T), customMessage);
