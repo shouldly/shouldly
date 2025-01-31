3224ccad14a3a20fdc6472379ccdf9fe6b3d20e6
DoNotTapeOver <58527684+DoNotTapeOver@users.noreply.github.com>
2025-01-31 15:29:39 -0800

    Added ShouldBeTrue and ShouldBeFalse methods for Nullable Boolean. (#1068)

--- src/Shouldly.Tests/ShouldBeBoolean/TrueScenario.cs
+++ src/Shouldly.Tests/ShouldBeBoolean/TrueScenario.cs
@@ -34,6 +34,70 @@ public class TrueScenario
     }
 
     [Fact]
+    public void NullableBooleanNullShouldFail()
+    {
+        bool? myValue = null;
+
+        Verify.ShouldFail(() =>
+                myValue.ShouldBeTrue("Some additional context"),
+
+            errorWithSource:
+            """
+            myValue
+                should be
+            True
+                but was
+            null
+
+            Additional Info:
+                Some additional context
+            """,
+
+            errorWithoutSource:
+            """
+            null
+                should be
+            True
+                but was not
+
+            Additional Info:
+                Some additional context
+            """);
+    }
+
+    [Fact]
+    public void NullableBooleanFalseShouldFail()
+    {
+        bool? myValue = false;
+
+        Verify.ShouldFail(() =>
+                myValue.ShouldBeTrue("Some additional context"),
+
+            errorWithSource:
+            """
+            myValue
+                should be
+            True
+                but was
+            False
+
+            Additional Info:
+                Some additional context
+            """,
+
+            errorWithoutSource:
+            """
+            False
+                should be
+            True
+                but was not
+
+            Additional Info:
+                Some additional context
+            """);
+    }
+
+    [Fact]
     public void ShouldPass()
     {
         true.ShouldBeTrue();
