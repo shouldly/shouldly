b3ad322b9a0820624be6b993af8d8048d9f6774d
DoNotTapeOver <58527684+DoNotTapeOver@users.noreply.github.com>
2025-01-31 22:19:32 -0800

    Added ShouldBeEmpty method for Guid (#1067)

---
+++ src/Shouldly.Tests/ShouldBeEmpty/GuidScenario.cs
@@ -0,0 +1,42 @@
+﻿namespace Shouldly.Tests.ShouldBeEmpty;
+
+public class GuidScenario
+{
+    [Fact]
+    public void ShouldFail()
+    {
+        Guid myGuid = new Guid(1,2,3,4,5,6,7,8,9,10,11);
+
+        Verify.ShouldFail(() =>
+                myGuid.ShouldBeEmpty("Some additional context"),
+
+            errorWithSource:
+            """
+            myGuid
+                should be empty
+            00000000-0000-0000-0000-000000000000
+                but was
+            00000001-0002-0003-0405-060708090a0b
+
+            Additional Info:
+                Some additional context
+            """,
+
+            errorWithoutSource:
+            """
+            00000001-0002-0003-0405-060708090a0b
+                should be empty
+            00000000-0000-0000-0000-000000000000
+                but was not
+            
+            Additional Info:
+                Some additional context
+            """);
+    }
+
+    [Fact]
+    public void ShouldPass()
+    {
+        Guid.Empty.ShouldBeEmpty();
+    }
+}
---
+++ src/Shouldly/ShouldlyExtensionMethods/ShouldBe/GuidShouldBeTestExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace Shouldly;
+
+[ShouldlyMethods]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static partial class GuidShouldBeTestExtensions
+{
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void ShouldBeEmpty(this Guid actual, string? customMessage = null)
+    {
+        actual.AssertAwesomely(v => Is.Equal(Guid.Empty, v), actual, Guid.Empty, customMessage);
+    }
+}
+
